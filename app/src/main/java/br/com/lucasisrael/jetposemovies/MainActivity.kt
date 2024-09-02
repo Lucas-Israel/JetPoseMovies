1d1d6dfea39cbdada9b628a54b163682be90b595
Lucas Israel <lisraelalves92@gmail.com>
2024-09-02 17:30:31 -0300

    another behavioral test

--- app/src/main/java/br/com/lucasisrael/jetposemovies/MainActivity.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,14 +12,19 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import br.com.lucasisrael.jetposemovies.genres.GenresViewModel
 import br.com.lucasisrael.jetposemovies.ui.theme.JetPoseMoviesTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val viewModel: GenresViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             JetPoseMoviesTheme {
+                viewModel.getFromRepository()
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
