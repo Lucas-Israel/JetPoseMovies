9796ce53d9deb9ad79cf246dd0f9fe9b2b225fa4
Lucas Israel <lisraelalves92@gmail.com>
2024-09-05 14:03:06 -0300

    calling NavGraph

--- app/src/main/java/br/com/lucasisrael/jetposemovies/MainActivity.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/MainActivity.kt
@@ -4,18 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.sp
+import br.com.lucasisrael.jetposemovies.common.navigation.NavGraph
 import br.com.lucasisrael.jetposemovies.ui.theme.JetPoseMoviesTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -26,24 +15,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             JetPoseMoviesTheme {
-                var count by remember{
-                    mutableIntStateOf(0)
-                }
-                Column(
-                    modifier = Modifier.fillMaxSize(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center
-                ) {
-                    Text(
-                        text = count.toString(),
-                        fontSize = 30.sp
-                    )
-                    Button(onClick = {
-                        count ++
-                    }) {
-                        Text(text = "Aumentar contador")
-                    }
-                }
+                NavGraph()
             }
         }
     }
