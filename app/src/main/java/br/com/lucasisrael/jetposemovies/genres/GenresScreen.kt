ac23ba3fde3f75002c609a8cda43e64bd9201e69
Lucas Israel <lisraelalves92@gmail.com>
2024-09-05 17:22:48 -0300

    WIP using hiltviewmodel to access viewmodel, some testing to show API response.

--- app/src/main/java/br/com/lucasisrael/jetposemovies/genres/GenresScreen.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/genres/GenresScreen.kt
@@ -1,43 +1,62 @@
 package br.com.lucasisrael.jetposemovies.genres
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import br.com.lucasisrael.jetposemovies.common.navigation.NavigationActions
 import kotlinx.serialization.Serializable
 
 @Serializable
 object GenresScreen
 
+@Suppress("FunctionNaming")
 @Composable
-fun GenresScreen(navigationActions: NavigationActions) {
+fun GenresScreen(
+    navigationActions: NavigationActions,
+    viewModel: GenresViewModel = hiltViewModel(),
+) {
+    val genres by viewModel.genres.collectAsStateWithLifecycle()
+
+    viewModel.getFromRepository()
 
     Surface(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier.fillMaxSize().padding(32.dp),
         color = MaterialTheme.colorScheme.background,
     ) {
-        Column(
+        LazyColumn(
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Text(
-                text = "Genres Screen",
-                modifier = Modifier.padding(16.dp)
-            )
-            Button(onClick = {
-                navigationActions.toMovieGenreScreen()
-            }) {
-                Text(text = "Go to Movies Genre Screen")
+            if (genres.genres.isNotEmpty()) {
+                genres.genres.forEach {
+                    item {
+                        Text(
+                            text = it.name,
+                            modifier = Modifier.padding(16.dp)
+                        )
+                    }
+                }
+            }
+            item {
+                Button(onClick = {
+                    navigationActions.toMovieGenreScreen()
+                }) {
+                    Text(text = "Go to Movies Genre Screen")
+                }
             }
         }
+
     }
 }
