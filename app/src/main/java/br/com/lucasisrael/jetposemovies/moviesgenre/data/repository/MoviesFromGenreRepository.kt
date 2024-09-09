67014fa6548eb1d2d12cc94aacb7a779c9832ba2
Lucas Israel <lisraelalves92@gmail.com>
2024-09-09 18:31:28 -0300

    MoviesFromGenreRepository

---
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/moviesgenre/data/repository/MoviesFromGenreRepository.kt
@@ -0,0 +1,8 @@
+package br.com.lucasisrael.jetposemovies.moviesgenre.data.repository
+
+import br.com.lucasisrael.jetposemovies.common.models.Resource
+import br.com.lucasisrael.jetposemovies.moviesgenre.data.respose.MoviesFromGenreResponse
+
+interface MoviesFromGenreRepository {
+    suspend fun getMoviesFromGenre(): Resource<MoviesFromGenreResponse?>
+}
---
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/moviesgenre/data/repository/MoviesFromGenreRepositoryImpl.kt
@@ -0,0 +1,19 @@
+package br.com.lucasisrael.jetposemovies.moviesgenre.data.repository
+
+import android.os.Build
+import androidx.annotation.RequiresExtension
+import br.com.lucasisrael.jetposemovies.common.coroutines.safeApiCall
+import br.com.lucasisrael.jetposemovies.common.models.Resource
+import br.com.lucasisrael.jetposemovies.moviesgenre.data.datasource.MoviesFromGenreDataSource
+import br.com.lucasisrael.jetposemovies.moviesgenre.data.respose.MoviesFromGenreResponse
+import javax.inject.Inject
+
+class MoviesFromGenreRepositoryImpl @Inject constructor(private val dataSource: MoviesFromGenreDataSource) :
+    MoviesFromGenreRepository {
+    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+    override suspend fun getMoviesFromGenre(): Resource<MoviesFromGenreResponse?> {
+        return safeApiCall {
+            dataSource.getMoviesFromGenre()
+        }
+    }
+}
