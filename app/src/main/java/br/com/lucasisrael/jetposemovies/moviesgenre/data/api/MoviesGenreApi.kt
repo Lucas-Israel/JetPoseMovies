cd66b5448d592da4aa785e561793575ed401525c
Lucas Israel <lisraelalves92@gmail.com>
2024-09-10 16:37:18 -0300

    MoviesGenreApi

--- app/src/main/java/br/com/lucasisrael/jetposemovies/moviesgenre/data/api/MoviesGenreApi.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/moviesgenre/data/api/MoviesGenreApi.kt
@@ -2,8 +2,9 @@ package br.com.lucasisrael.jetposemovies.moviesgenre.data.api
 
 import br.com.lucasisrael.jetposemovies.moviesgenre.data.respose.MoviesFromGenreResponse
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface MoviesGenreApi {
-    @GET("discover/movie?include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=28")
-    suspend fun getMoviesFromGenre(): MoviesFromGenreResponse
+    @GET("discover/movie")
+    suspend fun getMoviesFromGenre(@Query("with_genre") id: String): MoviesFromGenreResponse
 }
