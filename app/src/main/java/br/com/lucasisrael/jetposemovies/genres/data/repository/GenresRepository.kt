6ff77f9b557c8d6d336ef52f054ab2324969d153
Lucas Israel <lisraelalves92@gmail.com>
2024-09-10 18:49:03 -0300

    changing GenresRepository to fit GenresWithImgUrl

--- app/src/main/java/br/com/lucasisrael/jetposemovies/genres/data/repository/GenresRepository.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/genres/data/repository/GenresRepository.kt
@@ -1,9 +1,9 @@
 package br.com.lucasisrael.jetposemovies.genres.data.repository
 
+import br.com.lucasisrael.jetposemovies.common.models.GenresWithImgUrl
 import br.com.lucasisrael.jetposemovies.common.models.Resource
-import br.com.lucasisrael.jetposemovies.genres.data.response.GenresResponse
 
 interface GenresRepository {
 
-    suspend fun getGenres(): Resource<GenresResponse?>
+    suspend fun getGenres(): Resource<MutableList<GenresWithImgUrl>?>
 }
--- app/src/main/java/br/com/lucasisrael/jetposemovies/genres/data/repository/GenresRepositoryImpl.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/genres/data/repository/GenresRepositoryImpl.kt
@@ -3,17 +3,29 @@ package br.com.lucasisrael.jetposemovies.genres.data.repository
 import android.os.Build
 import androidx.annotation.RequiresExtension
 import br.com.lucasisrael.jetposemovies.common.coroutines.safeApiCall
+import br.com.lucasisrael.jetposemovies.common.models.GenresWithImgUrl
 import br.com.lucasisrael.jetposemovies.common.models.Resource
 import br.com.lucasisrael.jetposemovies.genres.data.datasource.GenresDataSource
-import br.com.lucasisrael.jetposemovies.genres.data.response.GenresResponse
+import br.com.lucasisrael.jetposemovies.moviesgenre.data.datasource.MoviesFromGenreDataSource
 import javax.inject.Inject
 
-class GenresRepositoryImpl @Inject constructor(private val dataSource: GenresDataSource): GenresRepository {
+class GenresRepositoryImpl @Inject constructor(
+    private val genresDataSource: GenresDataSource,
+    private val moviesFromGenreDataSource: MoviesFromGenreDataSource
+): GenresRepository {
 
     @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
-    override suspend fun getGenres(): Resource<GenresResponse?> {
+    override suspend fun getGenres(): Resource<MutableList<GenresWithImgUrl>?> {
         return safeApiCall {
-            dataSource.getGenres()
+            val payload: MutableList<GenresWithImgUrl> = mutableListOf()
+            val genres = genresDataSource.getGenres()
+            genres.genres.forEach {
+                val id = it.id.toString()
+                val movies = moviesFromGenreDataSource.getMoviesFromGenre(id)
+                payload.add(GenresWithImgUrl(it.id, it.name, movies.results[0].backdrop_path))
+            }
+
+            return@safeApiCall payload
         }
     }
 }
