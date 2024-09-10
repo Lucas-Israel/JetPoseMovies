96f8915702e57572a9ea7c52b6b98007bca495b2
Lucas Israel <lisraelalves92@gmail.com>
2024-09-10 18:50:50 -0300

    MoviesFromGenreViewModel added

---
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/moviesgenre/MoviesFromGenreViewModel.kt
@@ -0,0 +1,47 @@
+package br.com.lucasisrael.jetposemovies.moviesgenre
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import br.com.lucasisrael.jetposemovies.common.models.Resource
+import br.com.lucasisrael.jetposemovies.moviesgenre.data.repository.MoviesFromGenreRepository
+import br.com.lucasisrael.jetposemovies.moviesgenre.data.respose.MoviesFromGenreResponse
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MoviesFromGenreViewModel @Inject constructor(private val repository: MoviesFromGenreRepository) :
+    ViewModel() {
+
+    private val _isLoading = MutableStateFlow(true)
+    val isLoading: StateFlow<Boolean> = _isLoading.asStateFlow()
+
+    private val _moviesFromGenre = MutableStateFlow(MoviesFromGenreResponse(0, listOf(), 0, 0))
+    val moviesFromGenre: StateFlow<MoviesFromGenreResponse> = _moviesFromGenre.asStateFlow()
+
+    fun getMoviesFromGenreRepository(genreId: String) {
+        viewModelScope.launch {
+            try {
+                _isLoading.value = true
+                when (val response = repository.getMoviesFromGenre(genreId)) {
+                    is Resource.Success -> {
+                        _moviesFromGenre.value = response.data!!
+                    }
+
+                    is Resource.Error -> {
+
+                    }
+                }
+            } catch (e: CancellationException) {
+                e.printStackTrace()
+            }
+            finally {
+                _isLoading.value = false
+            }
+        }
+    }
+}
