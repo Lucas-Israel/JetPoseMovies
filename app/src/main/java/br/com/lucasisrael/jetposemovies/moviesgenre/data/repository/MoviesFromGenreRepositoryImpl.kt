0a4b098c7a774caf36a90b14b14e7d1e07e7c217
Lucas Israel <lisraelalves92@gmail.com>
2024-09-11 16:10:20 -0300

    refactor for renamed package

--- app/src/main/java/br/com/lucasisrael/jetposemovies/moviesgenre/data/repository/MoviesFromGenreRepositoryImpl.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/moviesgenre/data/repository/MoviesFromGenreRepositoryImpl.kt
@@ -5,10 +5,12 @@ import androidx.annotation.RequiresExtension
 import br.com.lucasisrael.jetposemovies.common.coroutines.safeApiCall
 import br.com.lucasisrael.jetposemovies.common.models.Resource
 import br.com.lucasisrael.jetposemovies.moviesgenre.data.datasource.MoviesFromGenreDataSource
-import br.com.lucasisrael.jetposemovies.moviesgenre.data.respose.MoviesFromGenreResponse
+import br.com.lucasisrael.jetposemovies.moviesgenre.data.response.MoviesFromGenreResponse
 import javax.inject.Inject
 
-class MoviesFromGenreRepositoryImpl @Inject constructor(private val dataSource: MoviesFromGenreDataSource) :
+class MoviesFromGenreRepositoryImpl @Inject constructor(
+    private val dataSource: MoviesFromGenreDataSource,
+) :
     MoviesFromGenreRepository {
     @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
     override suspend fun getMoviesFromGenre(id: String): Resource<MoviesFromGenreResponse?> {
