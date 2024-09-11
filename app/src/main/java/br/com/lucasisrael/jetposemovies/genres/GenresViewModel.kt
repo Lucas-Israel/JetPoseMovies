01ef6ce57b38ca181528a86ae35c90aabf7fd837
Lucas Israel <lisraelalves92@gmail.com>
2024-09-11 16:08:56 -0300

    added CoroutinesProvider in an effort to reduce main thread load and added search logic

--- app/src/main/java/br/com/lucasisrael/jetposemovies/genres/GenresViewModel.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/genres/GenresViewModel.kt
@@ -2,6 +2,7 @@ package br.com.lucasisrael.jetposemovies.genres
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import br.com.lucasisrael.jetposemovies.common.coroutines.CoroutinesProvider
 import br.com.lucasisrael.jetposemovies.common.models.GenresWithImgUrl
 import br.com.lucasisrael.jetposemovies.common.models.Resource
 import br.com.lucasisrael.jetposemovies.genres.data.repository.GenresRepository
@@ -14,31 +15,66 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class GenresViewModel @Inject constructor(private val repository: GenresRepository) : ViewModel() {
+class GenresViewModel @Inject constructor(
+    private val repository: GenresRepository,
+    private val coroutinesProvider: CoroutinesProvider
+) : ViewModel() {
 
     private val _isLoading = MutableStateFlow(true)
     val isLoading: StateFlow<Boolean> = _isLoading.asStateFlow()
 
     private val _genres =
-        MutableStateFlow(mutableListOf(GenresWithImgUrl(0, "", "")))
-    val genres: StateFlow<MutableList<GenresWithImgUrl>> = _genres.asStateFlow()
+        MutableStateFlow<List<GenresWithImgUrl>>(listOf())
+    val genres: StateFlow<List<GenresWithImgUrl>> = _genres.asStateFlow()
 
-    fun getFromRepository() {
-        viewModelScope.launch {
+    private var cachedGenresList = listOf<GenresWithImgUrl>()
+    private var isSearchStarting = true
+
+
+    init {
+        getFromRepository()
+    }
+
+    fun searchGenres(query: String) {
+        val listToSearch = if (isSearchStarting) {
+            _genres.value
+        } else {
+            cachedGenresList
+        }
+        viewModelScope.launch(coroutinesProvider.default()) {
+            if (query.isEmpty()) {
+                _genres.value = cachedGenresList
+                isSearchStarting = true
+                return@launch
+            }
+            val results = listToSearch.filter {
+                it.name.contains(query.trim(), ignoreCase = true)
+            }
+            if (isSearchStarting) {
+                cachedGenresList = _genres.value
+                isSearchStarting = false
+            }
+            _genres.value = results
+        }
+    }
+
+    private fun getFromRepository() {
+        viewModelScope.launch(coroutinesProvider.io()) {
             try {
                 _isLoading.value = true
                 when (val response = repository.getGenres()) {
                     is Resource.Success -> {
                         _genres.value = response.data!!
-                        _isLoading.value = false
                     }
 
                     is Resource.Error -> {
-                        _isLoading.value = false
+
                     }
                 }
             } catch (e: CancellationException) {
                 e.printStackTrace()
+            } finally {
+                _isLoading.value = false
             }
         }
     }
