68ec38bc20a4299957d9268c2488eed8888136e8
Lucas Israel <lisraelalves92@gmail.com>
2024-09-11 16:09:57 -0300

    using CustomCard that was moved from here, using searchbar

--- app/src/main/java/br/com/lucasisrael/jetposemovies/genres/GenresScreen.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/genres/GenresScreen.kt
@@ -2,31 +2,17 @@ package br.com.lucasisrael.jetposemovies.genres
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import br.com.lucasisrael.jetposemovies.common.ui.components.SearchBar
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import br.com.lucasisrael.jetposemovies.common.navigation.NavigationActions
+import br.com.lucasisrael.jetposemovies.common.ui.components.CustomCard
 import br.com.lucasisrael.jetposemovies.common.ui.screen.ScreenStructure
-import coil.compose.AsyncImage
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -38,18 +24,14 @@ fun GenresScreen(
     navigationActions: NavigationActions,
     viewModel: GenresViewModel = hiltViewModel(),
 ) {
-    viewModel.getFromRepository()
 
     val collectingGenres by viewModel.genres.collectAsStateWithLifecycle()
 
     ScreenStructure {
         SearchBar(
-            hint = "Search...",
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
+            hint = "Search..."
         ) {
-
+            viewModel.searchGenres(it)
         }
         LazyColumn(
             verticalArrangement = Arrangement.Center,
@@ -58,42 +40,14 @@ fun GenresScreen(
             if (collectingGenres.isNotEmpty()) {
                 collectingGenres.forEach {
                     item {
-                        Box(
-                            contentAlignment = Alignment.Center,
+                        CustomCard(
+                            title = it.name,
+                            url = it.imgUrl!!,
                             modifier = Modifier
-                                .fillMaxSize()
                                 .clickable {
                                     navigationActions.toMovieGenreScreen()
                                 }
-                                .padding(8.dp)
-                        ) {
-
-                            Card(
-                                modifier = Modifier
-                                    .fillMaxWidth(),
-                                elevation = CardDefaults.cardElevation(10.dp),
-                                colors = CardDefaults.cardColors(
-                                    containerColor = Color.DarkGray
-                                )
-                            ) {
-                                Column(
-                                    modifier = Modifier.fillMaxSize()
-                                ) {
-                                    AsyncImage(
-                                        model = "https://image.tmdb.org/t/p/w500${it.imgUrl}",
-                                        contentDescription = "image of a movie from the ${it.name} genre",
-                                        contentScale = ContentScale.FillBounds,
-                                        modifier = Modifier.height(400.dp)
-                                    )
-                                    Text(
-                                        text = it.name,
-                                        modifier = Modifier.padding(10.dp),
-                                        fontWeight = FontWeight.Bold,
-                                        fontSize = 24.sp,
-                                    )
-                                }
-                            }
-                        }
+                        )
                     }
                 }
             }
