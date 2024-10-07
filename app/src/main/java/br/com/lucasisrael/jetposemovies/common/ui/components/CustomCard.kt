1cfe7d83acb7503405768809c858cd975f6b5933
Lucas Israel <lisraelalves92@gmail.com>
2024-10-07 13:55:59 -0300

    adding placeholder and error for images

--- app/src/main/java/br/com/lucasisrael/jetposemovies/common/ui/components/CustomCard.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/common/ui/components/CustomCard.kt
@@ -13,9 +13,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import br.com.lucasisrael.jetposemovies.R
 import coil.compose.AsyncImage
 
 @SuppressWarnings("FunctionNaming")
@@ -48,6 +50,8 @@ fun CustomCard(
                     contentDescription = "Movie image from the $title genre",
                     contentScale = ContentScale.Crop,
                     modifier = Modifier.fillMaxWidth(),
+                    placeholder = painterResource(id = R.drawable.ic_launcher_foreground),
+                    error = painterResource(id = R.drawable.ic_action_name)
                 )
                 Text(
                     text = title,
