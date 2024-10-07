e228e9fd83376050de79f1626aa93aca78e607e3
Lucas Israel <lisraelalves92@gmail.com>
2024-10-07 16:51:50 -0300

    searchBar logic in MoviesFromGenreViewModel

--- app/src/main/java/br/com/lucasisrael/jetposemovies/moviesgenre/MoviesFromGenreViewModel.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/moviesgenre/MoviesFromGenreViewModel.kt
@@ -6,6 +6,7 @@ import br.com.lucasisrael.jetposemovies.common.coroutines.CoroutinesProvider
 import br.com.lucasisrael.jetposemovies.common.models.Resource
 import br.com.lucasisrael.jetposemovies.moviesgenre.data.repository.MoviesFromGenreRepository
 import br.com.lucasisrael.jetposemovies.moviesgenre.data.response.MoviesFromGenreResponse
+import br.com.lucasisrael.jetposemovies.moviesgenre.data.response.MoviesFromGenreResult
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -18,14 +19,49 @@ import javax.inject.Inject
 class MoviesFromGenreViewModel @Inject constructor(
     private val repository: MoviesFromGenreRepository,
     private val coroutinesProvider: CoroutinesProvider
-) :
-    ViewModel() {
+) : ViewModel() {
 
     private val _isLoading = MutableStateFlow(true)
     val isLoading: StateFlow<Boolean> = _isLoading.asStateFlow()
 
-    private val _moviesFromGenre = MutableStateFlow(MoviesFromGenreResponse(0, listOf(), 0, 0))
+    private val _moviesFromGenre =
+        MutableStateFlow(MoviesFromGenreResponse(0, listOf(), 0, 0))
     val moviesFromGenre: StateFlow<MoviesFromGenreResponse> = _moviesFromGenre.asStateFlow()
+
+    private var cachedMoviesFromGenre =
+        MoviesFromGenreResponse(0, listOf<MoviesFromGenreResult>(), 0, 0)
+    private var isSearchStarting = true
+
+    fun searchMoviesFromGenre(query: String) {
+        val listToSearch = if (isSearchStarting) {
+            _moviesFromGenre.value
+        } else {
+            cachedMoviesFromGenre
+        }
+        viewModelScope.launch(coroutinesProvider.default()) {
+            if (query.isEmpty()) {
+                _moviesFromGenre.value = cachedMoviesFromGenre
+                isSearchStarting = true
+                return@launch
+            }
+            val results = listToSearch.results.filter {
+                it.title.contains(query.trim(), ignoreCase = true)
+            }
+
+            val totalResult = MoviesFromGenreResponse(
+                _moviesFromGenre.value.page,
+                results,
+                _moviesFromGenre.value.total_pages,
+                _moviesFromGenre.value.total_results
+            )
+
+            if (isSearchStarting) {
+                cachedMoviesFromGenre = _moviesFromGenre.value
+                isSearchStarting = false
+            }
+            _moviesFromGenre.value = totalResult
+        }
+    }
 
     fun getMoviesFromGenreRepository(genreId: String) {
         viewModelScope.launch(coroutinesProvider.io()) {
