2417191d639074023c61e08376d7669fcd35cd8f
Lucas Israel <lisraelalves92@gmail.com>
2024-10-07 16:53:20 -0300

    using searchBar in MoviesGenreScreen
    added Text on top of the screen to indicate what genre is being displayed

--- app/src/main/java/br/com/lucasisrael/jetposemovies/moviesgenre/MoviesGenreScreen.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/moviesgenre/MoviesGenreScreen.kt
@@ -3,21 +3,29 @@ package br.com.lucasisrael.jetposemovies.moviesgenre
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import br.com.lucasisrael.jetposemovies.R
 import br.com.lucasisrael.jetposemovies.common.navigation.NavigationActions
 import br.com.lucasisrael.jetposemovies.common.ui.components.CustomCard
+import br.com.lucasisrael.jetposemovies.common.ui.components.SearchBar
 import br.com.lucasisrael.jetposemovies.common.ui.screen.LoadingScreen
 import br.com.lucasisrael.jetposemovies.common.ui.screen.ScreenStructure
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class MoviesGenreScreen(
-    val genreId: String
+    val genreId: String,
+    val genreName: String,
 )
 
 @Suppress("FunctionNaming")
@@ -25,9 +33,13 @@ data class MoviesGenreScreen(
 fun MoviesGenreScreen(
     navigationActions: NavigationActions,
     viewModel: MoviesFromGenreViewModel = hiltViewModel(),
-    genreId: String
+    genreId: String,
+    genreName: String
 ) {
-    viewModel.getMoviesFromGenreRepository(genreId)
+    LaunchedEffect(Unit) {
+        viewModel.getMoviesFromGenreRepository(genreId)
+    }
+
     val collectingMoviesGenre by viewModel.moviesFromGenre.collectAsStateWithLifecycle()
     val isLoading by viewModel.isLoading.collectAsStateWithLifecycle()
 
@@ -35,13 +47,22 @@ fun MoviesGenreScreen(
         LoadingScreen()
     } else {
         ScreenStructure {
+            Text(
+                text = stringResource(R.string.movies, genreName),
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Bold
+            )
+
+            SearchBar {
+                viewModel.searchMoviesFromGenre(it)
+            }
+
             LazyColumn(
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally,
             ) {
-                val results = collectingMoviesGenre.results
-                if (results.isNotEmpty()) {
-                    results.forEach {
+                if (collectingMoviesGenre.results.isNotEmpty()) {
+                    collectingMoviesGenre.results.forEach {
                         item {
                             CustomCard(
                                 title = it.title,
