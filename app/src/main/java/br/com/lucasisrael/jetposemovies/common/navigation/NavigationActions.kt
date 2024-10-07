98cf0bcf7695fc25b4a53c43b5ec85dd4ab2136a
Lucas Israel <lisraelalves92@gmail.com>
2024-10-07 11:31:08 -0300

    navigation actions refactor to pass arguments

--- app/src/main/java/br/com/lucasisrael/jetposemovies/common/navigation/NavigationActions.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/common/navigation/NavigationActions.kt
@@ -2,14 +2,14 @@ package br.com.lucasisrael.jetposemovies.common.navigation
 
 import androidx.navigation.NavHostController
 import br.com.lucasisrael.jetposemovies.common.navigation.Screens.DETAILS_SCREEN
-import br.com.lucasisrael.jetposemovies.common.navigation.Screens.MOVIES_GENRE_SCREEN
+import br.com.lucasisrael.jetposemovies.moviesgenre.MoviesGenreScreen
 
 class NavigationActions(
     private val navController: NavHostController,
 ) {
 
-    fun toMovieGenreScreen() {
-        navController.navigate(MOVIES_GENRE_SCREEN)
+    fun toMovieGenreScreen(genreId: String) {
+        navController.navigate(MoviesGenreScreen(genreId))
     }
 
     fun toDetailsScreen() {
