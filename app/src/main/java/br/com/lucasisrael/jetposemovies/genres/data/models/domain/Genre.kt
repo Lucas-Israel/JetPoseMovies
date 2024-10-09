ee0c5e351034e78bd867ea6ab4f887a7cb4b876a
Lucas Israel <lisraelalves92@gmail.com>
2024-10-09 15:12:27 -0300

    renamed to singular

--- app/src/main/java/br/com/lucasisrael/jetposemovies/genres/data/models/domain/Genre.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/genres/data/models/domain/Genre.kt
@@ -1,6 +1,6 @@
 package br.com.lucasisrael.jetposemovies.genres.data.models.domain
 
-data class Genres(
+data class Genre(
     val id: Int,
     val name: String
 )
--- app/src/main/java/br/com/lucasisrael/jetposemovies/genres/data/models/remote/GenreDto.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/genres/data/models/remote/GenreDto.kt
@@ -1,6 +1,6 @@
 package br.com.lucasisrael.jetposemovies.genres.data.models.remote
 
-data class GenresDto(
+data class GenreDto(
     val id: Int,
     val name: String
 )
