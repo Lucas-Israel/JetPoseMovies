ed486d09029e80d1c5199cb2be3a1d8c42a283b7
Lucas Israel <lisraelalves92@gmail.com>
2024-10-09 14:50:15 -0300

    refactoring to use moved code, return logic from response to List<GenresDto> and accessing genres field from response

--- app/src/main/java/br/com/lucasisrael/jetposemovies/genres/data/datasource/GenresDataSourceImpl.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/genres/data/datasource/GenresDataSourceImpl.kt
@@ -1,12 +1,12 @@
 package br.com.lucasisrael.jetposemovies.genres.data.datasource
 
 import br.com.lucasisrael.jetposemovies.genres.data.api.GenresApi
-import br.com.lucasisrael.jetposemovies.genres.data.response.GenresResponse
+import br.com.lucasisrael.jetposemovies.genres.data.models.remote.GenresDto
 import javax.inject.Inject
 
 class GenresDataSourceImpl @Inject constructor(private val api: GenresApi): GenresDataSource {
 
-    override suspend fun getGenres(): GenresResponse {
-        return api.getGenres()
+    override suspend fun getGenres(): List<GenresDto> {
+        return api.getGenres().genres
     }
 }
