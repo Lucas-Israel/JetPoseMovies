f677220388f515155155ec7a62e66db6e06b0a89
Lucas Israel <lisraelalves92@gmail.com>
2024-10-10 17:04:35 -0300

    DI for GenreDao and GenreDataBase

---
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/genres/di/GenresDataBaseModule.kt
@@ -0,0 +1,36 @@
+package br.com.lucasisrael.jetposemovies.genres.di
+
+import android.content.Context
+import androidx.room.Room
+import androidx.room.withTransaction
+import br.com.lucasisrael.jetposemovies.genres.data.datasource.database.GenreDao
+import br.com.lucasisrael.jetposemovies.genres.data.datasource.database.GenreDataBase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object GenresDataBaseModule {
+
+    @Provides
+    @Singleton
+    fun provideGenreDataBase(@ApplicationContext appContext: Context): GenreDataBase {
+        return Room.databaseBuilder(
+            appContext,
+            GenreDataBase::class.java,
+            "genre_database"
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    suspend fun provideGenreDao(dataBase: GenreDataBase): GenreDao {
+        return dataBase.withTransaction {
+            dataBase.genreDao()
+        }
+    }
+}
