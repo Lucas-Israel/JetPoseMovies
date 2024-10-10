cb448a19b2c01bb3e7a34aee236e7a242f66241e
Lucas Israel <lisraelalves92@gmail.com>
2024-10-10 17:07:36 -0300

    changed GenresRepository.getGenresWithImage return from MutableList to List

--- app/src/main/java/br/com/lucasisrael/jetposemovies/genres/data/repository/GenresRepository.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/genres/data/repository/GenresRepository.kt
@@ -2,8 +2,9 @@ package br.com.lucasisrael.jetposemovies.genres.data.repository
 
 import br.com.lucasisrael.jetposemovies.common.models.GenresWithImgUrl
 import br.com.lucasisrael.jetposemovies.common.models.Resource
+import br.com.lucasisrael.jetposemovies.genres.data.models.local.GenreEntity
 
 interface GenresRepository {
-
-    suspend fun getGenres(): Resource<MutableList<GenresWithImgUrl>?>
+    suspend fun getGenresWithImage(): Resource<List<GenresWithImgUrl>?>
+    suspend fun refreshGenres()
 }
