4954082d06e7b26e8264b9f93e843ddc6bb19b87
Lucas Israel <lisraelalves92@gmail.com>
2024-10-10 17:08:32 -0300

    refactoring GenresRepositoryImpl.getGenresWithImage return, adding refreshGenres function

--- app/src/main/java/br/com/lucasisrael/jetposemovies/genres/data/repository/GenresRepositoryImpl.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/genres/data/repository/GenresRepositoryImpl.kt
@@ -1,31 +1,51 @@
 package br.com.lucasisrael.jetposemovies.genres.data.repository
 
 import android.os.Build
+import android.util.Log
 import androidx.annotation.RequiresExtension
 import br.com.lucasisrael.jetposemovies.common.coroutines.safeApiCall
 import br.com.lucasisrael.jetposemovies.common.models.GenresWithImgUrl
 import br.com.lucasisrael.jetposemovies.common.models.Resource
-import br.com.lucasisrael.jetposemovies.genres.data.datasource.GenresDataSource
+import br.com.lucasisrael.jetposemovies.genres.data.datasource.database.GenreDao
+import br.com.lucasisrael.jetposemovies.genres.data.datasource.local.GenresLocal
+import br.com.lucasisrael.jetposemovies.genres.data.datasource.remote.GenresRemote
+import br.com.lucasisrael.jetposemovies.genres.data.mappers.toGenresEntity
+import br.com.lucasisrael.jetposemovies.genres.data.models.local.GenreEntity
 import br.com.lucasisrael.jetposemovies.moviesgenre.data.datasource.MoviesFromGenreDataSource
 import javax.inject.Inject
 
 class GenresRepositoryImpl @Inject constructor(
-    private val genresDataSource: GenresDataSource,
-    private val moviesFromGenreDataSource: MoviesFromGenreDataSource
+    private val genresRemote: GenresRemote,
+    private val moviesFromGenreDataSource: MoviesFromGenreDataSource,
+    private val genresLocal: GenresLocal
 ) : GenresRepository {
 
     @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
-    override suspend fun getGenres(): Resource<MutableList<GenresWithImgUrl>?> {
+    override suspend fun getGenresWithImage(): Resource<List<GenresWithImgUrl>?> {
+
         return safeApiCall {
-            val payload: MutableList<GenresWithImgUrl> = mutableListOf()
-            val genres = genresDataSource.getGenres()
-            genres.forEach {
+            refreshGenres()
+            val genres = genresLocal.getGenresFromDataBase()
+
+            genres.map {
                 val id = it.id.toString()
                 val movies = moviesFromGenreDataSource.getMoviesFromGenre(id, page = 1)
-                payload.add(GenresWithImgUrl(it.id, it.name, movies.results[0].poster_path))
+                val imageUrlForGenre = movies.results[0].poster_path
+                GenresWithImgUrl(it.id, it.name, imageUrlForGenre)
             }
+        }
+    }
 
-            return@safeApiCall payload
+    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+    override suspend fun refreshGenres() {
+
+        safeApiCall {
+            val genresRemote = genresRemote.getGenres()
+            genresLocal.saveGenresToDataBase(
+                genresRemote.map {
+                    it.toGenresEntity()
+                }
+            )
         }
     }
 }
