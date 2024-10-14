d0fb71de865b9f3d12091979bb489d3e7e765937
Lucas Israel <lisraelalves92@gmail.com>
2024-10-14 16:03:07 -0300

    MoviesFromGenreRepositoryImpl now saving from remote to local and using from local

--- app/src/main/java/br/com/lucasisrael/jetposemovies/moviesgenre/data/repository/MoviesFromGenreRepositoryImpl.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/moviesgenre/data/repository/MoviesFromGenreRepositoryImpl.kt
@@ -4,18 +4,32 @@ import android.os.Build
 import androidx.annotation.RequiresExtension
 import br.com.lucasisrael.jetposemovies.common.coroutines.safeApiCall
 import br.com.lucasisrael.jetposemovies.common.models.Resource
-import br.com.lucasisrael.jetposemovies.moviesgenre.data.datasource.MoviesFromGenreDataSource
-import br.com.lucasisrael.jetposemovies.moviesgenre.data.models.response.MoviesFromGenreResponse
+import br.com.lucasisrael.jetposemovies.moviesgenre.data.datasource.local.MoviesFromGenreLocal
+import br.com.lucasisrael.jetposemovies.moviesgenre.data.datasource.remote.MoviesFromGenreRemote
+import br.com.lucasisrael.jetposemovies.moviesgenre.data.mappers.toMoviesFromGenreEntity
+import br.com.lucasisrael.jetposemovies.moviesgenre.data.models.local.MoviesFromGenreEntity
 import javax.inject.Inject
 
+@RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
 class MoviesFromGenreRepositoryImpl @Inject constructor(
-    private val dataSource: MoviesFromGenreDataSource,
-) :
-    MoviesFromGenreRepository {
+    private val moviesRemote: MoviesFromGenreRemote,
+    private val moviesLocal: MoviesFromGenreLocal,
+) : MoviesFromGenreRepository {
+
     @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
-    override suspend fun getMoviesFromGenre(id: String, page: Int): Resource<MoviesFromGenreResponse?> {
+    override suspend fun getMoviesFromGenre(genreId: String, page: Int): Resource<MoviesFromGenreEntity?> {
         return safeApiCall {
-            dataSource.getMoviesFromGenre(id, page)
+            refreshMoviesFromGenre(genreId, page)
+            return@safeApiCall moviesLocal.getMoviesFromGenreFromDataBase()
+        }
+    }
+
+    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+    override suspend fun refreshMoviesFromGenre(genreId: String, page: Int) {
+        safeApiCall {
+            moviesLocal.saveMoviesFromGenreToDataBase(
+                moviesRemote.getMoviesFromGenre(genreId, page).toMoviesFromGenreEntity()
+            )
         }
     }
 }
