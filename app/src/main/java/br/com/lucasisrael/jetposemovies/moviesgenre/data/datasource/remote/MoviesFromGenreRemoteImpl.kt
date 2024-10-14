68a45f6a179536863fccff5a8913e08a08a95bf9
Lucas Israel <lisraelalves92@gmail.com>
2024-10-14 13:24:12 -0300

    MoviesFromGenreRepositoryImpl moved and renamed from datasource to remote

--- app/src/main/java/br/com/lucasisrael/jetposemovies/moviesgenre/data/datasource/remote/MoviesFromGenreRemoteImpl.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/moviesgenre/data/datasource/remote/MoviesFromGenreRemoteImpl.kt
@@ -1,11 +1,11 @@
-package br.com.lucasisrael.jetposemovies.moviesgenre.data.datasource
+package br.com.lucasisrael.jetposemovies.moviesgenre.data.datasource.remote
 
 import br.com.lucasisrael.jetposemovies.moviesgenre.data.api.MoviesGenreApi
 import br.com.lucasisrael.jetposemovies.moviesgenre.data.models.response.MoviesFromGenreResponse
 import javax.inject.Inject
 
-class MoviesFromGenreDataSourceImpl @Inject constructor(private val api: MoviesGenreApi) :
-    MoviesFromGenreDataSource {
+class MoviesFromGenreRemoteImpl @Inject constructor(private val api: MoviesGenreApi) :
+    MoviesFromGenreRemote {
     override suspend fun getMoviesFromGenre(id: String, page: Int): MoviesFromGenreResponse {
         return api.getMoviesFromGenre(id = id, page = page)
     }
