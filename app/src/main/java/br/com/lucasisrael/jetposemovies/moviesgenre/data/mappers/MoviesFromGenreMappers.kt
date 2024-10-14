b2c4621dccbdabcf7df91868dee4fd1ea7aae2d6
Lucas Israel <lisraelalves92@gmail.com>
2024-10-14 18:45:41 -0300

    added MoviesFromGenreResponse.toMoviesFromGenreDto() in MoviesFromGenreMappers.kt

--- app/src/main/java/br/com/lucasisrael/jetposemovies/moviesgenre/data/mappers/MoviesFromGenreMappers.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/moviesgenre/data/mappers/MoviesFromGenreMappers.kt
@@ -3,6 +3,13 @@ package br.com.lucasisrael.jetposemovies.moviesgenre.data.mappers
 import br.com.lucasisrael.jetposemovies.moviesgenre.data.models.domain.MoviesFromGenre
 import br.com.lucasisrael.jetposemovies.moviesgenre.data.models.local.MoviesFromGenreEntity
 import br.com.lucasisrael.jetposemovies.moviesgenre.data.models.remote.MoviesFromGenreDto
+import br.com.lucasisrael.jetposemovies.moviesgenre.data.models.response.MoviesFromGenreResponse
+
+fun MoviesFromGenreResponse.toMoviesFromGenreDto(): MoviesFromGenreDto {
+    return MoviesFromGenreDto(
+        page, results, total_pages, total_results
+    )
+}
 
 fun MoviesFromGenreDto.toMoviesFromGenreEntity(): MoviesFromGenreEntity {
     return MoviesFromGenreEntity(
