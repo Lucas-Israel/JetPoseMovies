b35e7ce62609892ca991c2540e2ea5df7073c560
Lucas Israel <lisraelalves92@gmail.com>
2024-10-14 15:43:45 -0300

    refactoring import name, parameter name and return type changed to MoviesFromGenreDto

--- app/src/main/java/br/com/lucasisrael/jetposemovies/moviesgenre/data/datasource/remote/MoviesFromGenreRemoteImpl.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/moviesgenre/data/datasource/remote/MoviesFromGenreRemoteImpl.kt
@@ -1,12 +1,12 @@
 package br.com.lucasisrael.jetposemovies.moviesgenre.data.datasource.remote
 
 import br.com.lucasisrael.jetposemovies.moviesgenre.data.api.MoviesGenreApi
-import br.com.lucasisrael.jetposemovies.moviesgenre.data.models.response.MoviesFromGenreResponse
+import br.com.lucasisrael.jetposemovies.moviesgenre.data.models.remote.MoviesFromGenreDto
 import javax.inject.Inject
 
 class MoviesFromGenreRemoteImpl @Inject constructor(private val api: MoviesGenreApi) :
     MoviesFromGenreRemote {
-    override suspend fun getMoviesFromGenre(id: String, page: Int): MoviesFromGenreResponse {
-        return api.getMoviesFromGenre(id = id, page = page)
+    override suspend fun getMoviesFromGenre(genreId: String, page: Int): MoviesFromGenreDto {
+        return api.getMoviesFromGenre(genreId = genreId, page = page)
     }
 }
