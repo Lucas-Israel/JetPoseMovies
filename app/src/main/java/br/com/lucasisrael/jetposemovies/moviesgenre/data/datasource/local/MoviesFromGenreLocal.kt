d36799af6439b45379b69db5b881ac6ea2228c35
Lucas Israel <lisraelalves92@gmail.com>
2024-10-14 13:22:32 -0300

    MoviesFromGenreLocal changing entity to DTO

--- app/src/main/java/br/com/lucasisrael/jetposemovies/moviesgenre/data/datasource/local/MoviesFromGenreLocal.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/moviesgenre/data/datasource/local/MoviesFromGenreLocal.kt
@@ -1,8 +1,9 @@
 package br.com.lucasisrael.jetposemovies.moviesgenre.data.datasource.local
 
 import br.com.lucasisrael.jetposemovies.moviesgenre.data.models.local.MovieFromGenreEntity
+import br.com.lucasisrael.jetposemovies.moviesgenre.data.models.remote.MovieFromGenreDto
 
 interface MoviesFromGenreLocal {
-    suspend fun saveMoviesFromGenreToDataBase(movies: List<MovieFromGenreEntity>)
+    suspend fun saveMoviesFromGenreToDataBase(movies: List<MovieFromGenreDto>)
     suspend fun getMoviesFromGenreFromDataBase(): List<MovieFromGenreEntity>
 }
