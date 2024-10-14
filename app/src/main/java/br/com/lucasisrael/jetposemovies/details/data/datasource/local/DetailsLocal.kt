8097546a7cee5d42957e649d2ba3a4edce733b0d
Lucas Israel <lisraelalves92@gmail.com>
2024-10-14 18:32:16 -0300

    renaming function in DetailsLocal interface

--- app/src/main/java/br/com/lucasisrael/jetposemovies/details/data/datasource/local/DetailsLocal.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/details/data/datasource/local/DetailsLocal.kt
@@ -3,6 +3,6 @@ package br.com.lucasisrael.jetposemovies.details.data.datasource.local
 import br.com.lucasisrael.jetposemovies.details.data.models.local.DetailsEntity
 
 interface DetailsLocal {
-    suspend fun saveDetails(details: DetailsEntity)
+    suspend fun saveDetailsToDataBase(details: DetailsEntity)
     suspend fun getDetailsFromDataBase(): DetailsEntity
 }
