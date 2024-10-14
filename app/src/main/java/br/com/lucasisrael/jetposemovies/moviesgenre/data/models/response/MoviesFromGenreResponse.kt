0eca9cb8b36279427366c53de77cba5498a6b04e
Lucas Israel <lisraelalves92@gmail.com>
2024-10-14 15:37:11 -0300

    updated models for MoviesFromGenre

--- app/src/main/java/br/com/lucasisrael/jetposemovies/moviesgenre/data/models/response/MoviesFromGenreResponse.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/moviesgenre/data/models/response/MoviesFromGenreResponse.kt
@@ -1,10 +1,11 @@
 package br.com.lucasisrael.jetposemovies.moviesgenre.data.models.response
 
-import br.com.lucasisrael.jetposemovies.moviesgenre.data.models.remote.MovieFromGenreDto
+import br.com.lucasisrael.jetposemovies.common.models.Movie
+
 
 data class MoviesFromGenreResponse(
     val page: Int,
-    val results: List<MovieFromGenreDto>,
+    val results: List<Movie>,
     val total_pages: Int,
     val total_results: Int
 )
