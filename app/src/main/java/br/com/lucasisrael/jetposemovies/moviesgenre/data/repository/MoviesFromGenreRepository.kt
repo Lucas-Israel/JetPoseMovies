e98e7ae265326265fd5b1dacc638af83c83332a4
Lucas Israel <lisraelalves92@gmail.com>
2024-10-14 13:23:24 -0300

    MoviesFromGenreRepository interface

--- app/src/main/java/br/com/lucasisrael/jetposemovies/moviesgenre/data/repository/MoviesFromGenreRepository.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/moviesgenre/data/repository/MoviesFromGenreRepository.kt
@@ -1,8 +1,10 @@
 package br.com.lucasisrael.jetposemovies.moviesgenre.data.repository
 
 import br.com.lucasisrael.jetposemovies.common.models.Resource
+import br.com.lucasisrael.jetposemovies.moviesgenre.data.models.local.MovieFromGenreEntity
 import br.com.lucasisrael.jetposemovies.moviesgenre.data.models.response.MoviesFromGenreResponse
 
 interface MoviesFromGenreRepository {
-    suspend fun getMoviesFromGenre(id: String, page: Int): Resource<MoviesFromGenreResponse?>
+    suspend fun getMoviesFromGenre(): Resource<MoviesFromGenreResponse?>
+    suspend fun refreshMoviesFromGenre()
 }
