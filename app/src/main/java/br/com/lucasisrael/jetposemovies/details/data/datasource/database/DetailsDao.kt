09c6603240ec78b888f6c0bccb794a922c3b3029
Lucas Israel <lisraelalves92@gmail.com>
2024-10-14 18:09:33 -0300

    DetailsDao.kt

---
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/details/data/datasource/database/DetailsDao.kt
@@ -0,0 +1,18 @@
+package br.com.lucasisrael.jetposemovies.details.data.datasource.database
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import br.com.lucasisrael.jetposemovies.details.data.models.local.DetailsEntity
+
+@Dao
+interface DetailsDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertDetails(details: DetailsEntity)
+
+    @Query("SELECT * from detailsentity")
+    suspend fun getDetails()
+
+}
