eba82d0789208ef1ba06d74e40aee762d2a0b91d
Lucas Israel <lisraelalves92@gmail.com>
2024-10-14 15:42:54 -0300

    MoviesFromGenreMappers.kt
    changed to reflect its proper return

--- app/src/main/java/br/com/lucasisrael/jetposemovies/moviesgenre/data/mappers/MoviesFromGenreMappers.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/moviesgenre/data/mappers/MoviesFromGenreMappers.kt
@@ -1,43 +1,17 @@
 package br.com.lucasisrael.jetposemovies.moviesgenre.data.mappers
 
-import br.com.lucasisrael.jetposemovies.moviesgenre.data.models.domain.MovieFromGenre
-import br.com.lucasisrael.jetposemovies.moviesgenre.data.models.local.MovieFromGenreEntity
-import br.com.lucasisrael.jetposemovies.moviesgenre.data.models.remote.MovieFromGenreDto
+import br.com.lucasisrael.jetposemovies.moviesgenre.data.models.domain.MoviesFromGenre
+import br.com.lucasisrael.jetposemovies.moviesgenre.data.models.local.MoviesFromGenreEntity
+import br.com.lucasisrael.jetposemovies.moviesgenre.data.models.remote.MoviesFromGenreDto
 
-fun MovieFromGenreDto.toMoviesFromGenreEntity(): MovieFromGenreEntity {
-    return MovieFromGenreEntity(
-        adult = adult,
-        backdrop_path = backdrop_path,
-        genre_ids = genre_ids,
-        id = id,
-        original_language = original_language,
-        original_Title = original_Title,
-        overview = overview,
-        popularity = popularity,
-        poster_path = poster_path,
-        release_date = release_date,
-        title = title,
-        video = video,
-        vote_average = vote_average,
-        vote_count = vote_count
+fun MoviesFromGenreDto.toMoviesFromGenreEntity(): MoviesFromGenreEntity {
+    return MoviesFromGenreEntity(
+        page, results, total_pages, total_results
     )
 }
 
-fun MovieFromGenreEntity.toMovieFromGenre(): MovieFromGenre {
-    return MovieFromGenre(
-        adult = adult,
-        backdrop_path = backdrop_path,
-        genre_ids = genre_ids,
-        id = id,
-        original_language = original_language,
-        original_Title = original_Title,
-        overview = overview,
-        popularity = popularity,
-        poster_path = poster_path,
-        release_date = release_date,
-        title = title,
-        video = video,
-        vote_average = vote_average,
-        vote_count = vote_count
+fun MoviesFromGenreEntity.toMoviesFromGenre(): MoviesFromGenre {
+    return MoviesFromGenre(
+        page, results, total_pages, total_results
     )
 }
