30a6f0808a855960859bde51ced51b271a3f471e
Lucas Israel <lisraelalves92@gmail.com>
2024-10-14 19:09:53 -0300

    DetailsViewModel

---
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/details/presentation/DetailsViewModel.kt
@@ -0,0 +1,49 @@
+package br.com.lucasisrael.jetposemovies.details.presentation
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import br.com.lucasisrael.jetposemovies.common.coroutines.CoroutinesProvider
+import br.com.lucasisrael.jetposemovies.common.models.Resource
+import br.com.lucasisrael.jetposemovies.details.data.models.domain.Details
+import br.com.lucasisrael.jetposemovies.details.data.repository.DetailsRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class DetailsViewModel @Inject constructor(
+    private val repository: DetailsRepository,
+    private val coroutinesProvider: CoroutinesProvider
+) : ViewModel() {
+
+    private val _isLoading = MutableStateFlow(true)
+    val isLoading: StateFlow<Boolean> = _isLoading.asStateFlow()
+
+    private val _details = MutableStateFlow<Details?>(null)
+    val details: StateFlow<Details?> = _details.asStateFlow()
+
+    private fun getFromRepository(movieId: String) {
+        viewModelScope.launch(coroutinesProvider.io()) {
+            try {
+                _isLoading.value = true
+                when (val response = repository.getDetails(movieId)) {
+                    is Resource.Success -> {
+                        _details.value = response.data
+                    }
+
+                    is Resource.Error -> {
+                        _details.value = null
+                    }
+                }
+            } catch (e: CancellationException) {
+                e.printStackTrace()
+            } finally {
+                _isLoading.value = false
+            }
+        }
+    }
+}
