a8a42848665f03be64927397b1a8411403789405
Lucas Israel <lisraelalves92@gmail.com>
2024-10-15 19:44:05 -0300

    added TypeConverters for DetailsDataBase and exportSchema false

--- app/src/main/java/br/com/lucasisrael/jetposemovies/details/data/datasource/database/DetailsDataBase.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/details/data/datasource/database/DetailsDataBase.kt
@@ -5,14 +5,27 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-import br.com.lucasisrael.jetposemovies.details.data.datasource.converters.DetailsTypeConverters
+import br.com.lucasisrael.jetposemovies.common.converters.DetailConverters
+import br.com.lucasisrael.jetposemovies.common.converters.GenreConverters
+import br.com.lucasisrael.jetposemovies.common.converters.ISOCountryConverters
+import br.com.lucasisrael.jetposemovies.common.converters.MovieCollectionConverters
+import br.com.lucasisrael.jetposemovies.common.converters.ProductionCompanyConverters
+import br.com.lucasisrael.jetposemovies.common.converters.SpokenLanguagesConverters
 import br.com.lucasisrael.jetposemovies.details.data.models.local.DetailsEntity
 
 @Database(
     entities = [DetailsEntity::class],
-    version = 1
+    version = 1,
+    exportSchema = false
 )
-@TypeConverters(DetailsTypeConverters::class)
+@TypeConverters(
+    DetailConverters::class,
+    MovieCollectionConverters::class,
+    GenreConverters::class,
+    ProductionCompanyConverters::class,
+    ISOCountryConverters::class,
+    SpokenLanguagesConverters::class,
+)
 abstract class DetailsDataBase : RoomDatabase() {
 
     abstract fun detailsDao(): DetailsDao
@@ -26,7 +39,7 @@ abstract class DetailsDataBase : RoomDatabase() {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     DetailsDataBase::class.java,
-                    "details_database"
+                    "details_structure"
                 ).build()
                 INSTANCE = instance
                 instance
