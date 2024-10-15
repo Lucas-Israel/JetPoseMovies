8ddc717e6702d776a5e7198997c4fb7de631fc5b
Lucas Israel <lisraelalves92@gmail.com>
2024-10-15 14:32:26 -0300

    refactored unneeded code into one file

--- app/src/main/java/br/com/lucasisrael/jetposemovies/details/data/datasource/converters/DetailsTypeConverters.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/details/data/datasource/converters/DetailsTypeConverters.kt
@@ -1,17 +1,18 @@
 package br.com.lucasisrael.jetposemovies.details.data.datasource.converters
 
 import androidx.room.TypeConverter
+import br.com.lucasisrael.jetposemovies.common.converters.GenericTypeConverter
 import br.com.lucasisrael.jetposemovies.common.models.MovieCollection
 
 class DetailsTypeConverters {
 
     @TypeConverter
     fun fromMovieCollection(movieCollection: MovieCollection): String {
-        return movieCollectionToString(movieCollection)
+        return GenericTypeConverter().fromObject(movieCollection, MovieCollection::class)
     }
 
     @TypeConverter
     fun toMovieCollection(json: String): MovieCollection {
-        return stringToMovieCollection(json)
+        return GenericTypeConverter().toObject(json, MovieCollection::class)
     }
 }
