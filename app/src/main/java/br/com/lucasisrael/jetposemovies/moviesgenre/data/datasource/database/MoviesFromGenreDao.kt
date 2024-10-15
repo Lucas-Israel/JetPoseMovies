3f90b584f21e76a1a13314a2908f6f3b5d3f87f8
Lucas Israel <lisraelalves92@gmail.com>
2024-10-15 19:35:16 -0300

    changing table name in MoviesFromGenreDao and insertMoviesFromGenre() parameter type from Dto to Entity

--- app/src/main/java/br/com/lucasisrael/jetposemovies/moviesgenre/data/datasource/database/MoviesFromGenreDao.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/moviesgenre/data/datasource/database/MoviesFromGenreDao.kt
@@ -6,20 +6,19 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import br.com.lucasisrael.jetposemovies.moviesgenre.data.models.local.MoviesFromGenreEntity
-import br.com.lucasisrael.jetposemovies.moviesgenre.data.models.remote.MoviesFromGenreDto
 
 @Dao
 interface MoviesFromGenreDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertMoviesFromGenre(moviesFromGenre: MoviesFromGenreDto)
+    suspend fun insertMoviesFromGenre(moviesFromGenre: MoviesFromGenreEntity)
 
-    @Query("SELECT * from moviesFromGenreEntity")
+    @Query("SELECT * from movies_from_genre")
     suspend fun getAllMoviesFromGenre(): MoviesFromGenreEntity
 
     @Delete
     suspend fun deleteMovieFromGenre(movieFromGenre: MoviesFromGenreEntity)
 
-    @Query("DELETE from moviesFromGenreEntity")
+    @Query("DELETE from movies_from_genre")
     suspend fun clearAll()
 }
