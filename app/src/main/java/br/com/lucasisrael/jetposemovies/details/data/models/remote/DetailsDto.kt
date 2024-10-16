690e947458d1f164a824944425ddfa624dd78fe0
Lucas Israel <lisraelalves92@gmail.com>
2024-10-16 17:55:36 -0300

    removing unused import

--- app/src/main/java/br/com/lucasisrael/jetposemovies/details/data/models/remote/DetailsDto.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/details/data/models/remote/DetailsDto.kt
@@ -4,7 +4,6 @@ import br.com.lucasisrael.jetposemovies.common.models.ISOCountry
 import br.com.lucasisrael.jetposemovies.common.models.MovieCollection
 import br.com.lucasisrael.jetposemovies.common.models.ProductionCompany
 import br.com.lucasisrael.jetposemovies.common.models.SpokenLanguages
-import br.com.lucasisrael.jetposemovies.genres.data.models.local.GenreEntity
 import br.com.lucasisrael.jetposemovies.genres.data.models.remote.GenreDto
 
 data class DetailsDto(
