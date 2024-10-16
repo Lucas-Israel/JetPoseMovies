6dc7dbda7d38e1d50cf0cdf5d2197e3889b259a6
Lucas Israel <lisraelalves92@gmail.com>
2024-10-16 13:37:03 -0300

    add: default params for MovieCollection data class

--- app/src/main/java/br/com/lucasisrael/jetposemovies/common/models/MovieCollection.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/common/models/MovieCollection.kt
@@ -3,10 +3,10 @@ package br.com.lucasisrael.jetposemovies.common.models
 import androidx.room.ColumnInfo
 
 data class MovieCollection(
-    val id: Int,
-    val name: String,
+    val id: Int = 0,
+    val name: String = "",
     @ColumnInfo(name = "poster_path")
-    val posterPath: String,
+    val posterPath: String = "",
     @ColumnInfo(name = "backdrop_path")
-    val backdropPath: String
+    val backdropPath: String = ""
 )
