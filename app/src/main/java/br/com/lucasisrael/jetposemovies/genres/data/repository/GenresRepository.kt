de2467ec134a39c30db61b08ed3426eaa90bcb19
Lucas Israel <lisraelalves92@gmail.com>
2024-10-17 16:10:17 -0300

    renamed and added functions to improve GenresRepository logic

--- app/src/main/java/br/com/lucasisrael/jetposemovies/genres/data/repository/GenresRepository.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/genres/data/repository/GenresRepository.kt
@@ -1,10 +1,11 @@
 package br.com.lucasisrael.jetposemovies.genres.data.repository
 
-import br.com.lucasisrael.jetposemovies.common.models.GenresWithImgUrl
 import br.com.lucasisrael.jetposemovies.common.models.Resource
 import br.com.lucasisrael.jetposemovies.genres.data.models.local.GenreEntity
+import br.com.lucasisrael.jetposemovies.genres.data.models.remote.GenreDto
 
 interface GenresRepository {
-    suspend fun getGenresWithImage(): Resource<List<GenresWithImgUrl>?>
-    suspend fun refreshGenres()
+    suspend fun getGenresFromApi(): Resource<List<GenreDto>?>
+    suspend fun saveGenresToDb(genres: List<GenreDto>)
+    suspend fun loadGenresFromDb(): List<GenreEntity>
 }
