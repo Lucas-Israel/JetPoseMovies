7a2e453f8dc65c409b40271beaae882354e28c83
Lucas Israel <lisraelalves92@gmail.com>
2024-10-17 16:08:09 -0300

    added additional mappers in GenresMappers.kt

--- app/src/main/java/br/com/lucasisrael/jetposemovies/genres/data/mappers/GenresMappers.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/genres/data/mappers/GenresMappers.kt
@@ -1,8 +1,17 @@
 package br.com.lucasisrael.jetposemovies.genres.data.mappers
 
+import br.com.lucasisrael.jetposemovies.common.models.GenreWithImgUrl
 import br.com.lucasisrael.jetposemovies.genres.data.models.domain.Genre
 import br.com.lucasisrael.jetposemovies.genres.data.models.local.GenreEntity
 import br.com.lucasisrael.jetposemovies.genres.data.models.remote.GenreDto
+import br.com.lucasisrael.jetposemovies.genres.data.models.response.GenreResponse
+
+fun GenreResponse.toGenreDto() : GenreDto {
+    return GenreDto(
+        id = id,
+        name = name
+    )
+}
 
 fun GenreDto.toGenresEntity() : GenreEntity {
     return GenreEntity(
@@ -15,5 +24,13 @@ fun GenreEntity.toGenres() : Genre {
     return Genre(
         id = id,
         name = name
+    )
+}
+
+fun GenreEntity.toGenreWithUrl(imageUrl: String) : GenreWithImgUrl {
+    return GenreWithImgUrl(
+        id = id,
+        name = name,
+        imgUrl = imageUrl
     )
 }
