bf1306aec0c65ab031eba01cfef5df427b9f350a
Lucas Israel <lisraelalves92@gmail.com>
2024-10-17 16:22:03 -0300

    improved functions for GenresRepositoryImpl

--- app/src/main/java/br/com/lucasisrael/jetposemovies/genres/data/repository/GenresRepositoryImpl.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/genres/data/repository/GenresRepositoryImpl.kt
@@ -3,46 +3,30 @@ package br.com.lucasisrael.jetposemovies.genres.data.repository
 import android.os.Build
 import androidx.annotation.RequiresExtension
 import br.com.lucasisrael.jetposemovies.common.coroutines.safeApiCall
-import br.com.lucasisrael.jetposemovies.common.models.GenresWithImgUrl
 import br.com.lucasisrael.jetposemovies.common.models.Resource
 import br.com.lucasisrael.jetposemovies.genres.data.datasource.local.GenresLocal
 import br.com.lucasisrael.jetposemovies.genres.data.datasource.remote.GenresRemote
 import br.com.lucasisrael.jetposemovies.genres.data.mappers.toGenresEntity
-import br.com.lucasisrael.jetposemovies.moviesgenre.data.datasource.remote.MoviesFromGenreRemote
+import br.com.lucasisrael.jetposemovies.genres.data.models.local.GenreEntity
+import br.com.lucasisrael.jetposemovies.genres.data.models.remote.GenreDto
 import javax.inject.Inject
 
 class GenresRepositoryImpl @Inject constructor(
     private val genresRemote: GenresRemote,
-    private val moviesFromGenreDataSource: MoviesFromGenreRemote,
     private val genresLocal: GenresLocal
 ) : GenresRepository {
 
     @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
-    override suspend fun getGenresWithImage(): Resource<List<GenresWithImgUrl>?> {
+    override suspend fun getGenresFromApi(): Resource<List<GenreDto>?> {
+        return safeApiCall { genresRemote.getGenres() }
+    }
 
-        return safeApiCall {
-            refreshGenres()
-            val genres = genresLocal.getGenresFromDataBase()
-
-            genres.map {
-                val id = it.id.toString()
-                val movies = moviesFromGenreDataSource.getMoviesFromGenre(id, page = 1)
-                val imageUrlForGenre = movies.results[0].posterPath
-                GenresWithImgUrl(it.id, it.name, imageUrlForGenre)
-            }
-        }
+    override suspend fun saveGenresToDb(genres: List<GenreDto>) {
+        genresLocal.saveGenresToDataBase(genres.map { it.toGenresEntity() })
     }
 
     @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
-    override suspend fun refreshGenres() {
-
-        safeApiCall {
-            val genresRemote = genresRemote.getGenres()
-            genresLocal.saveGenresToDataBase(
-                genresRemote.map {
-                    it.toGenresEntity()
-                }
-            )
-        }
+    override suspend fun loadGenresFromDb(): List<GenreEntity> {
+        return genresLocal.getGenresFromDataBase()
     }
 }
