37f32e8bf5413d251dfb6014ac05194b7eb4f7ad
Lucas Israel <lisraelalves92@gmail.com>
2024-10-17 16:06:14 -0300

    renamed and rewritten: to represent a single element

---
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/genres/data/models/response/GenreResponse.kt
@@ -0,0 +1,6 @@
+package br.com.lucasisrael.jetposemovies.genres.data.models.response
+
+data class GenreResponse(
+    val id: Int,
+    val name: String
+)
--- app/src/main/java/br/com/lucasisrael/jetposemovies/genres/data/models/response/GenresResponse.kt
+++
@@ -1,7 +0,0 @@
-package br.com.lucasisrael.jetposemovies.genres.data.models.response
-
-import br.com.lucasisrael.jetposemovies.genres.data.models.remote.GenreDto
-
-data class GenresResponse(
-    val genres: List<GenreDto>
-)
