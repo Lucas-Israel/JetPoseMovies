dd46489986cba7e42daacc5643d9686ed3d59ccf
Lucas Israel <lisraelalves92@gmail.com>
2024-10-18 16:19:45 -0300

    removing unused function added .replaceNullable when creating an entity

--- app/src/main/java/br/com/lucasisrael/jetposemovies/genres/data/mappers/GenresMappers.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/genres/data/mappers/GenresMappers.kt
@@ -1,22 +1,15 @@
 package br.com.lucasisrael.jetposemovies.genres.data.mappers
 
-import br.com.lucasisrael.jetposemovies.common.models.GenreWithImgUrl
-import br.com.lucasisrael.jetposemovies.genres.data.models.domain.Genre
+import br.com.lucasisrael.jetposemovies.common.utils.types.values.dephault.replaceNullable
+import br.com.lucasisrael.jetposemovies.genres.domain.models.GenreWithImgUrl
 import br.com.lucasisrael.jetposemovies.genres.data.models.local.GenreEntity
 import br.com.lucasisrael.jetposemovies.genres.data.models.remote.GenreDto
-import br.com.lucasisrael.jetposemovies.genres.data.models.response.GenreResponse
-
-fun GenreResponse.toGenreDto() : GenreDto {
-    return GenreDto(
-        id = id,
-        name = name
-    )
-}
+import br.com.lucasisrael.jetposemovies.genres.domain.models.Genre
 
 fun GenreDto.toGenresEntity() : GenreEntity {
     return GenreEntity(
-        id = id,
-        name = name
+        id.replaceNullable(),
+        name.replaceNullable()
     )
 }
 
