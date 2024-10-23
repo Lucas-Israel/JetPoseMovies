91afda955f5c556443d4d0424cfd485782c76d50
Lucas Israel <lisraelalves92@gmail.com>
2024-10-23 14:05:19 -0300

    DetailsViewModel using UseCase instead of repository

--- app/src/main/java/br/com/lucasisrael/jetposemovies/details/presentation/DetailsViewModel.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/details/presentation/DetailsViewModel.kt
@@ -3,9 +3,8 @@ package br.com.lucasisrael.jetposemovies.details.presentation
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import br.com.lucasisrael.jetposemovies.common.coroutines.CoroutinesProvider
-import br.com.lucasisrael.jetposemovies.common.models.Resource
-import br.com.lucasisrael.jetposemovies.details.data.models.domain.Details
-import br.com.lucasisrael.jetposemovies.details.data.repository.DetailsRepository
+import br.com.lucasisrael.jetposemovies.details.domain.models.Details
+import br.com.lucasisrael.jetposemovies.details.domain.usecase.DetailsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -16,12 +15,15 @@ import javax.inject.Inject
 
 @HiltViewModel
 class DetailsViewModel @Inject constructor(
-    private val repository: DetailsRepository,
+    private val useCase: DetailsUseCase,
     private val coroutinesProvider: CoroutinesProvider
 ) : ViewModel() {
 
     private val _isLoading = MutableStateFlow(true)
     val isLoading: StateFlow<Boolean> = _isLoading.asStateFlow()
+
+    private val _isError = MutableStateFlow(false)
+    val isError: StateFlow<Boolean> = _isError
 
     private val _details = MutableStateFlow<Details>(Details())
     val details: StateFlow<Details> = _details.asStateFlow()
@@ -30,18 +32,13 @@ class DetailsViewModel @Inject constructor(
         viewModelScope.launch(coroutinesProvider.io()) {
             try {
                 _isLoading.value = true
-                when (val response = repository.getDetails(movieId)) {
-                    is Resource.Success -> {
-                        _details.value = response.data!!
-                    }
-
-                    is Resource.Error -> {
-                        _details.value = Details()
-                    }
-                }
+                _isError.value = false
+                _details.value = useCase.invoke(movieId)
 
             } catch (e: CancellationException) {
                 e.printStackTrace()
+                _details.value = Details()
+                _isError.value = true
             } finally {
                 _isLoading.value = false
             }
