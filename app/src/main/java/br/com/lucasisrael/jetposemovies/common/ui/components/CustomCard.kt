ed030ec9c0f3907e76b6fc824d0ab938d6f73671
Lucas Israel <lisraelalves92@gmail.com>
2024-10-23 17:33:03 -0300

    using CustomAsyncImageWithGradient

--- app/src/main/java/br/com/lucasisrael/jetposemovies/common/ui/components/CustomCard.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/common/ui/components/CustomCard.kt
@@ -13,17 +13,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.drawWithCache
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import br.com.lucasisrael.jetposemovies.R
-import coil.compose.AsyncImage
 
 @SuppressWarnings("FunctionNaming")
 @Composable
@@ -35,7 +28,6 @@ fun CustomCard(
 ) {
 
     val color = Color.Black
-    val halfFloat = 0.5f
 
     Box(
         contentAlignment = Alignment.Center, modifier = modifier
@@ -58,26 +50,11 @@ fun CustomCard(
                     .padding(top = 8.dp, start = 8.dp, end = 8.dp)
             ) {
 
-                AsyncImage(
-                    model = stringResource(R.string.image_base_url, url!!),
-                    contentDescription = stringResource(R.string.movie_image_from_the_genre, title),
-                    contentScale = ContentScale.Crop,
-                    placeholder = painterResource(id = R.drawable.ic_launcher_foreground),
-                    error = painterResource(id = R.drawable.ic_action_name),
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .clip(RoundedCornerShape(topStart = 8.dp, topEnd = 8.dp))
-                        .drawWithCache {
-                            onDrawWithContent {
-                                drawContent()
-                                drawRect(
-                                    Brush.verticalGradient(
-                                        halfFloat to color.copy(alpha = 0f),
-                                        1f to color
-                                    )
-                                )
-                            }
-                        }
+                CustomAsyncImageWithGradient(
+                    url = url!!,
+                    title = title,
+                    modifier = modifier,
+                    color = color
                 )
 
                 if (rating != null) {
