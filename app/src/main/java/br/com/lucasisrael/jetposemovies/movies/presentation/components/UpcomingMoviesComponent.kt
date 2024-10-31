661c16fab9f1d4a6e7f79762ab3fbe7cd3814072
Lucas Israel <lisraelalves92@gmail.com>
2024-10-31 19:45:43 -0300

    added UpcomingMoviesComponent

---
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/movies/presentation/components/UpcomingMoviesComponent.kt
@@ -0,0 +1,35 @@
+package br.com.lucasisrael.jetposemovies.movies.presentation.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import br.com.lucasisrael.jetposemovies.common.navigation.NavigationActions
+import br.com.lucasisrael.jetposemovies.common.presentation.components.Carousel
+import br.com.lucasisrael.jetposemovies.common.presentation.screens.LoadingScreen
+import br.com.lucasisrael.jetposemovies.movies.data.datasource.local.SearchType
+import br.com.lucasisrael.jetposemovies.movies.presentation.viewmodels.MoviesListViewModel
+
+@SuppressWarnings("FunctionNaming")
+@Composable
+fun UpcomingMoviesComponent(
+    navigationActions: NavigationActions,
+    viewModel: MoviesListViewModel = hiltViewModel(),
+) {
+    LaunchedEffect(Unit) {
+        viewModel.getMoviesFromGenreRepository(searchType = SearchType.Upcoming)
+    }
+
+    val collectingMovies by viewModel.movieDomain.collectAsStateWithLifecycle()
+    val isLoading by viewModel.isLoading.collectAsStateWithLifecycle()
+
+    if (isLoading) {
+        LoadingScreen()
+    } else {
+        Carousel(
+            items = collectingMovies,
+            navigationActions = navigationActions
+        )
+    }
+}
