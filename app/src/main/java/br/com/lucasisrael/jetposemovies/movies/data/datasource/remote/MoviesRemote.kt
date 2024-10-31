df97065fa052e58eca9577e93302d3a7de55c34d
Lucas Israel <lisraelalves92@gmail.com>
2024-10-31 19:43:04 -0300

    removing nullable

--- app/src/main/java/br/com/lucasisrael/jetposemovies/movies/data/datasource/remote/MoviesRemote.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/movies/data/datasource/remote/MoviesRemote.kt
@@ -3,6 +3,6 @@ package br.com.lucasisrael.jetposemovies.movies.data.datasource.remote
 import br.com.lucasisrael.jetposemovies.movies.data.models.remote.MovieDto
 
 interface MoviesRemote {
-    suspend fun fetchMoviesFromGenre(genreId: String, page: Int): List<MovieDto?>
+    suspend fun fetchMoviesFromGenre(genreId: String, page: Int): List<MovieDto>
     suspend fun fetchUpcomingMovies(page: Int): List<MovieDto>
 }
