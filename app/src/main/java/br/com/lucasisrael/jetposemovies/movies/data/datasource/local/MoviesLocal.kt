2c45cd2077b8cfde57fca0b7b31bb378a9c19972
Lucas Israel <lisraelalves92@gmail.com>
2024-10-31 19:16:43 -0300

    refactored MoviesLocal.getMoviesFromGenreFromDataBase to
    MoviesLocal.getMoviesFromDatabase that uses a searchType

--- app/src/main/java/br/com/lucasisrael/jetposemovies/movies/data/datasource/local/MoviesLocal.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/movies/data/datasource/local/MoviesLocal.kt
@@ -5,5 +5,5 @@ import br.com.lucasisrael.jetposemovies.movies.data.models.remote.MovieDto
 
 interface MoviesLocal {
     suspend fun saveMoviesToDataBase(movies: MovieDto)
-    suspend fun getMoviesFromGenreFromDataBase(genreId: String): List<MovieEntity>
+    suspend fun getMoviesFromDatabase(searchType: SearchType): List<MovieEntity>
 }
