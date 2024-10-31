1cf6cf0d7fb66013ccea2828aedc5106a0076e2e
Lucas Israel <lisraelalves92@gmail.com>
2024-10-31 19:43:48 -0300

    added LazyColumn to display multiple components

--- app/src/main/java/br/com/lucasisrael/jetposemovies/common/presentation/screens/InitialScreen.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/common/presentation/screens/InitialScreen.kt
@@ -1,8 +1,10 @@
 package br.com.lucasisrael.jetposemovies.common.presentation.screens
 
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import br.com.lucasisrael.jetposemovies.common.navigation.NavigationActions
 import br.com.lucasisrael.jetposemovies.genres.presentation.GenresComponent
+import br.com.lucasisrael.jetposemovies.movies.presentation.components.UpcomingMoviesComponent
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -14,6 +16,13 @@ fun InitialScreen(
     navigationActions: NavigationActions,
 ) {
     ScreenStructure {
-        GenresComponent(navigationActions)
+        LazyColumn {
+            item {
+                UpcomingMoviesComponent(navigationActions)
+            }
+            item {
+                GenresComponent(navigationActions)
+            }
+        }
     }
 }
