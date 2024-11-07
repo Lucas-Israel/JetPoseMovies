08f58ab441d31a00ef0c04f3db492f965e5f0375
Lucas Israel <lisraelalves92@gmail.com>
2024-11-07 16:33:28 -0300

    MovieListRepository is no longer an interface but an abstract class

--- app/src/main/java/br/com/lucasisrael/jetposemovies/movies/data/repository/MoviesListRepository.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/movies/data/repository/MoviesListRepository.kt
@@ -1,13 +1,12 @@
 package br.com.lucasisrael.jetposemovies.movies.data.repository
 
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.RemoteMediator
 import br.com.lucasisrael.jetposemovies.common.models.Resource
 import br.com.lucasisrael.jetposemovies.movies.data.datasource.local.SearchType
 import br.com.lucasisrael.jetposemovies.movies.data.models.local.MovieEntity
 import br.com.lucasisrael.jetposemovies.movies.data.models.query.MovieApiQuery
 import br.com.lucasisrael.jetposemovies.movies.data.models.remote.MovieDto
 
-interface MoviesListRepository {
-    suspend fun fetchMovies(query: MovieApiQuery): Resource<List<MovieDto>?>
-    suspend fun saveMoviesFromGenreToDataBase(movieDto: MovieDto)
-    suspend fun loadMoviesFromDataBase(searchType: SearchType): List<MovieEntity>
-}
+@OptIn(ExperimentalPagingApi::class)
+abstract class MoviesListRepository<T: Any>: RemoteMediator<Int, T>()
--- app/src/main/java/br/com/lucasisrael/jetposemovies/movies/data/repository/MoviesListRepositoryImpl.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/movies/data/repository/MoviesListRepositoryImpl.kt
@@ -2,33 +2,75 @@ package br.com.lucasisrael.jetposemovies.movies.data.repository
 
 import android.os.Build
 import androidx.annotation.RequiresExtension
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.LoadType
+import androidx.paging.PagingState
+import androidx.room.withTransaction
 import br.com.lucasisrael.jetposemovies.common.coroutines.safeApiCall
 import br.com.lucasisrael.jetposemovies.common.models.Resource
-import br.com.lucasisrael.jetposemovies.movies.data.datasource.local.MoviesLocal
-import br.com.lucasisrael.jetposemovies.movies.data.datasource.local.SearchType
+import br.com.lucasisrael.jetposemovies.movies.data.datasource.database.MoviesDataBase
 import br.com.lucasisrael.jetposemovies.movies.data.datasource.remote.MoviesRemote
+import br.com.lucasisrael.jetposemovies.movies.data.mappers.toMoviesListEntity
 import br.com.lucasisrael.jetposemovies.movies.data.models.local.MovieEntity
 import br.com.lucasisrael.jetposemovies.movies.data.models.query.MovieApiQuery
-import br.com.lucasisrael.jetposemovies.movies.data.models.remote.MovieDto
+import coil.network.HttpException
+import java.io.IOException
 import javax.inject.Inject
 
 @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
 class MoviesListRepositoryImpl @Inject constructor(
     private val moviesRemote: MoviesRemote,
-    private val moviesLocal: MoviesLocal,
-) : MoviesListRepository {
+    private val moviesDataBase: MoviesDataBase,
+) : MoviesListRepository<MovieEntity>() {
 
-    override suspend fun saveMoviesFromGenreToDataBase(movieDto: MovieDto) {
-        moviesLocal.saveMoviesToDataBase(movieDto)
+    private var query = MovieApiQuery()
+
+    fun setQuery(query: MovieApiQuery) {
+        this.query = query
     }
 
-    override suspend fun loadMoviesFromDataBase(searchType: SearchType): List<MovieEntity> {
-        return moviesLocal.getMoviesFromDatabase(searchType)
+    fun getQuery(): MovieApiQuery {
+        return this.query
     }
 
-    override suspend fun fetchMovies(query: MovieApiQuery): Resource<List<MovieDto>?> {
-        return safeApiCall {
-            moviesRemote.fetchMovies(query)
+    @ExperimentalPagingApi
+    override suspend fun load(
+        loadType: LoadType,
+        state: PagingState<Int, MovieEntity>
+    ): MediatorResult {
+        return try {
+
+            val movies = safeApiCall {
+                moviesRemote.fetchMovies(query)
+            }
+
+            moviesDataBase.withTransaction {
+                if (loadType == LoadType.REFRESH) {
+                    moviesDataBase.movieDao().clearAll()
+                }
+                when (movies) {
+                    is Resource.Success -> {
+                        val moviesEntity = movies.data!!.results.map { it.toMoviesListEntity() }
+
+                        moviesDataBase.movieDao().upsertAll(moviesEntity)
+
+                        MediatorResult.Success(
+                            endOfPaginationReached = movies.data.page > 500
+                        )
+
+                    }
+
+                    is Resource.Error -> {
+                        MediatorResult.Error(IOException(movies.message))
+                    }
+                }
+            }
+
+        } catch (e: IOException) {
+            MediatorResult.Error(e)
+        } catch (e: HttpException) {
+            MediatorResult.Error(e)
         }
     }
+
 }
