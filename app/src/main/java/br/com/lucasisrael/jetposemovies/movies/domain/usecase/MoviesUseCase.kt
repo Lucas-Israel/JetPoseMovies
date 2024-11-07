18bb2bf606f29e3e26bb17c979b73071b7125519
Lucas Israel <lisraelalves92@gmail.com>
2024-11-07 16:29:22 -0300

    MoviesUseCase checkpoint

--- app/src/main/java/br/com/lucasisrael/jetposemovies/movies/domain/usecase/MoviesUseCase.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/movies/domain/usecase/MoviesUseCase.kt
@@ -7,8 +7,8 @@ import br.com.lucasisrael.jetposemovies.movies.data.models.remote.MovieDto
 import br.com.lucasisrael.jetposemovies.movies.domain.models.MovieDomain
 
 interface MoviesUseCase {
-    suspend fun fetchMovies(movieApiQuery: MovieApiQuery): List<MovieDto>
-    suspend fun saveMovies(movies: List<MovieDto>)
-    suspend fun loadMovies(searchType: SearchType): List<MovieEntity>
+//    suspend fun fetchMovies(movieApiQuery: MovieApiQuery): List<MovieDto>
+//    suspend fun saveMovies(movies: List<MovieDto>)
+//    suspend fun loadMovies(searchType: SearchType): List<MovieEntity>
     suspend fun synchronizeMovies(searchType: SearchType): List<MovieDomain>
 }
--- app/src/main/java/br/com/lucasisrael/jetposemovies/movies/domain/usecase/MoviesUseCaseImpl.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/movies/domain/usecase/MoviesUseCaseImpl.kt
@@ -1,6 +1,7 @@
 package br.com.lucasisrael.jetposemovies.movies.domain.usecase
 
 import android.util.Log
+import androidx.paging.Pager
 import br.com.lucasisrael.jetposemovies.common.models.Resource
 import br.com.lucasisrael.jetposemovies.movies.data.datasource.local.SearchType
 import br.com.lucasisrael.jetposemovies.movies.data.mappers.toMovieDomain
@@ -13,30 +14,30 @@ import kotlinx.coroutines.CancellationException
 import javax.inject.Inject
 
 class MoviesUseCaseImpl @Inject constructor(
-    private val repository: MoviesListRepository
+    pager: Pager<Int, MovieEntity>
 ) : MoviesUseCase {
 
-    override suspend fun fetchMovies(movieApiQuery: MovieApiQuery): List<MovieDto> {
-        return when (val response = repository.fetchMovies(movieApiQuery)) {
-            is Resource.Success -> {
-                response.data!!.map { it }
-            }
-
-            is Resource.Error -> {
-                listOf<MovieDto>()
-            }
-        }
-    }
-
-    override suspend fun saveMovies(movies: List<MovieDto>) {
-        movies.forEach {
-            repository.saveMoviesFromGenreToDataBase(it)
-        }
-    }
-
-    override suspend fun loadMovies(searchType: SearchType): List<MovieEntity> {
-        return repository.loadMoviesFromDataBase(searchType)
-    }
+//    override suspend fun fetchMovies(movieApiQuery: MovieApiQuery): List<MovieDto> {
+//        return when (val response = repository.load(movieApiQuery)) {
+//            is Resource.Success -> {
+//                response.data!!.map { it }
+//            }
+//
+//            is Resource.Error -> {
+//                listOf<MovieDto>()
+//            }
+//        }
+//    }
+//
+//    override suspend fun saveMovies(movies: List<MovieDto>) {
+//        movies.forEach {
+//            repository.saveMoviesFromGenreToDataBase(it)
+//        }
+//    }
+//
+//    override suspend fun loadMovies(searchType: SearchType): List<MovieEntity> {
+//        return repository.loadMoviesFromDataBase(searchType)
+//    }
 
     override suspend fun synchronizeMovies(searchType: SearchType): List<MovieDomain> {
         return try {
