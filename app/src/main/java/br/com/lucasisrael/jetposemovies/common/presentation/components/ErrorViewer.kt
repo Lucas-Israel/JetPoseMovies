2820e0b709beceae6390c4f6e96ca73aba09bd0a
Lucas Israel <lisraelalves92@gmail.com>
2024-12-03 17:31:17 -0300

    added ErrorViewer

---
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/common/presentation/components/ErrorViewer.kt
@@ -0,0 +1,24 @@
+package br.com.lucasisrael.jetposemovies.common.presentation.components
+
+import android.widget.Toast
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.platform.LocalContext
+import androidx.paging.LoadState
+import androidx.paging.compose.LazyPagingItems
+import br.com.lucasisrael.jetposemovies.movies.domain.models.MovieDomain
+
+@Suppress("FunctionNaming")
+@Composable
+fun ErrorViewer(movies: LazyPagingItems<MovieDomain>) {
+    val context = LocalContext.current
+    LaunchedEffect(key1 = movies.loadState) {
+        if (movies.loadState.refresh is LoadState.Error) {
+            Toast.makeText(
+                context,
+                "Error: " + (movies.loadState.refresh as LoadState.Error).error.message,
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+}
