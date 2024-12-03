7fe00e77845510b7060499475453963ecfcbd87f
Lucas Israel <lisraelalves92@gmail.com>
2024-12-03 17:33:40 -0300

    reworked MovieRepository

---
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/movies/data/repository/MoviesRepository.kt
@@ -0,0 +1,19 @@
+package br.com.lucasisrael.jetposemovies.movies.data.repository
+
+import androidx.paging.PagingSource
+import br.com.lucasisrael.jetposemovies.common.utils.types.SearchType
+import br.com.lucasisrael.jetposemovies.movies.data.datasource.local.MoviesDao
+import br.com.lucasisrael.jetposemovies.movies.data.models.local.MovieEntity
+import javax.inject.Inject
+
+class MoviesRepository @Inject constructor(
+    private val movieDao: MoviesDao,
+) {
+    fun load(searchType: SearchType): PagingSource<Int, MovieEntity> {
+        return when (searchType) {
+            is SearchType.GenreId -> movieDao.moviesByGenrePagingSource(searchType.genreId)
+            is SearchType.Popular -> movieDao.popularMovies()
+            is SearchType.Upcoming -> movieDao.upcomingMovies()
+        }
+    }
+}
