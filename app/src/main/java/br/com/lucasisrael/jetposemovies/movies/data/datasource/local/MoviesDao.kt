4d087020a35d4b812ec7ca7fd52f96faec6cf5f5
Lucas Israel <lisraelalves92@gmail.com>
2024-12-03 18:37:05 -0300

    clearing unused functions and renaming a parameter

--- app/src/main/java/br/com/lucasisrael/jetposemovies/movies/data/datasource/local/MoviesDao.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/movies/data/datasource/local/MoviesDao.kt
@@ -13,27 +13,10 @@ import br.com.lucasisrael.jetposemovies.movies.data.models.local.MovieEntity
 interface MoviesDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertMoviesFromGenre(moviesFromGenre: MovieEntity)
+    suspend fun insertMoviesFromGenre(movie: MovieEntity)
 
     @Upsert
     suspend fun upsertAll(movies: List<MovieEntity>)
-
-    @Query(
-        "SELECT * from movies_from_genre" +
-                " WHERE genre_ids LIKE '%' || :genreId || '%'" +
-                " ORDER BY popularity DESC LIMIT 20"
-    )
-    suspend fun getAllMoviesFromGenre(genreId: String): List<MovieEntity>
-
-    @Query(
-        "SELECT * from movies_from_genre ORDER BY release_date DESC LIMIT 5"
-    )
-    suspend fun getUpcomingMovies(): List<MovieEntity>
-
-    @Query(
-        "SELECT * from movies_from_genre ORDER BY popularity DESC LIMIT 20"
-    )
-    suspend fun getPopularMovies(): List<MovieEntity>
 
     @Delete
     suspend fun deleteMovieFromGenre(movieFromGenre: MovieEntity)
