935dafdba9e5159beff6a6abbcbaac19ed237182
Lucas Israel <lisraelalves92@gmail.com>
2024-12-03 17:29:41 -0300

    removing unused imports and style changes in CustomCard

--- app/src/main/java/br/com/lucasisrael/jetposemovies/common/presentation/components/CustomCard.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/common/presentation/components/CustomCard.kt
@@ -1,38 +1,29 @@
 package br.com.lucasisrael.jetposemovies.common.presentation.components
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 
 @SuppressWarnings("FunctionNaming")
 @Composable
 fun CustomCard(
     title: String? = null,
     url: String? = null,
-    rating: Double? = null,
     modifier: Modifier,
 ) {
-
-    val color = Color.Black
 
     Box(
         contentAlignment = Alignment.Center, modifier = modifier
             .fillMaxSize()
-            .padding(8.dp)
+            .padding(4.dp)
     ) {
 
         Card(
@@ -42,51 +33,11 @@ fun CustomCard(
                 containerColor = Color.DarkGray
             )
         ) {
-
-            Box(
-                contentAlignment = Alignment.BottomEnd,
-                modifier = modifier
-                    .fillMaxSize()
-                    .padding(top = 8.dp, start = 8.dp, end = 8.dp)
-            ) {
-
-                CustomAsyncImageWithGradient(
-                    url = url,
-                    title = title,
-                    modifier = modifier,
-                    color = color
-                )
-
-                if (rating != null) {
-                    PercentageWheel(
-                        rating = rating
-                    )
-                }
-            }
-
-            if (title != null) {
-                Box(
-                    contentAlignment = Alignment.Center,
-                    modifier = modifier
-                        .fillMaxSize()
-                        .padding(bottom = 8.dp, start = 8.dp, end = 8.dp)
-                        .clip(
-                            RoundedCornerShape(
-                                bottomStart = 8.dp,
-                                bottomEnd = 8.dp
-                            )
-                        )
-                        .background(color)
-                ) {
-                    Text(
-                        text = title ?: "",
-                        fontWeight = FontWeight.Bold,
-                        fontSize = 24.sp,
-                        modifier = Modifier
-                            .padding(10.dp)
-                    )
-                }
-            }
+            CustomAsyncImage(
+                url = url,
+                title = title,
+                modifier = modifier,
+            )
         }
     }
 }
