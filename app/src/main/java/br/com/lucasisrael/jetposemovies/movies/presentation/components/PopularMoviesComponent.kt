e98578c687b83efb70e943d573b162f1c1f38414
Lucas Israel <lisraelalves92@gmail.com>
2024-12-05 15:19:55 -0300

    Using CustomLazyRow in PopularMoviesComponent and new stringResource

--- app/src/main/java/br/com/lucasisrael/jetposemovies/movies/presentation/components/PopularMoviesComponent.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/movies/presentation/components/PopularMoviesComponent.kt
@@ -1,25 +1,11 @@
 package br.com.lucasisrael.jetposemovies.movies.presentation.components
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.paging.LoadState
 import androidx.paging.compose.collectAsLazyPagingItems
 import br.com.lucasisrael.jetposemovies.R
 import br.com.lucasisrael.jetposemovies.common.navigation.NavigationActions
-import br.com.lucasisrael.jetposemovies.common.presentation.components.ErrorViewer
 import br.com.lucasisrael.jetposemovies.common.utils.types.SearchType
 import br.com.lucasisrael.jetposemovies.movies.presentation.viewmodel.MovieListViewModel
 
@@ -34,42 +20,7 @@ fun PopularMoviesComponent(
     viewModel.setMoviesFlow(searchType = SearchType.Popular(page = page))
     val movies = viewModel.popularMoviesPagingFlow.collectAsLazyPagingItems()
 
-    ErrorViewer(movies = movies)
+    val categoryText = stringResource(R.string.popular_movies)
 
-    Column(
-        modifier = Modifier
-            .padding(8.dp)
-    ) {
-        Text(
-            text = stringResource(R.string.popular_movies),
-            fontWeight = FontWeight.Bold,
-            fontSize = 18.sp
-        )
-            LazyRow {
-                items(count = movies.itemCount) { index ->
-                    val movie = movies[index]
-                    if (movie != null) {
-                        Box(
-                            modifier = Modifier
-                                .padding(4.dp)
-                        ) {
-                            if (movies.loadState.refresh is LoadState.Loading) {
-                                CircularProgressIndicator(
-                                    modifier = Modifier
-                                )
-                            } else {
-                            MovieItem(
-                                movie = movie,
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .clickable {
-                                        navigationActions.toDetailsScreen(movieId = movie.id.toString())
-                                    }
-                            )
-                        }
-                    }
-                }
-            }
-        }
-    }
+    CustomLazyRow(movies, navigationActions, categoryText)
 }
