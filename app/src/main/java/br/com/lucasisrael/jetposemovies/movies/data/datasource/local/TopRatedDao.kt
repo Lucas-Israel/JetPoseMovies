1d951113c84c2fb6fb591b30a9b9113776f5a3c6
Lucas Israel <lisraelalves92@gmail.com>
2024-12-17 18:42:05 -0300

    added TopRatedDao

---
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/movies/data/datasource/local/TopRatedDao.kt
@@ -0,0 +1,23 @@
+package br.com.lucasisrael.jetposemovies.movies.data.datasource.local
+
+import androidx.paging.PagingSource
+import androidx.room.Dao
+import androidx.room.Query
+import androidx.room.Transaction
+import androidx.room.Upsert
+import br.com.lucasisrael.jetposemovies.movies.data.models.local.TopRatedMoviesEntity
+import br.com.lucasisrael.jetposemovies.movies.data.models.local.TopRatedWithMovie
+
+@Dao
+interface TopRatedDao {
+
+    @Upsert
+    fun upsert(list: List<TopRatedMoviesEntity>)
+
+    @Query("SELECT * from top_rated")
+    @Transaction
+    fun load(): PagingSource<Int, TopRatedWithMovie>
+
+    @Query("DELETE from top_rated")
+    fun clearAll()
+}
