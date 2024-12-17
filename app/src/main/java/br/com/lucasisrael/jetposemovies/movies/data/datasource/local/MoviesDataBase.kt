7bd2898338edc0ec0ec1414a2897c40fbe1b66b7
Lucas Israel <lisraelalves92@gmail.com>
2024-12-17 17:58:05 -0300

    added upcomingdao and populardao to database

--- app/src/main/java/br/com/lucasisrael/jetposemovies/movies/data/datasource/local/MoviesDataBase.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/movies/data/datasource/local/MoviesDataBase.kt
@@ -5,17 +5,19 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import br.com.lucasisrael.jetposemovies.common.converters.ListConverters
 import br.com.lucasisrael.jetposemovies.movies.data.models.local.MovieEntity
+import br.com.lucasisrael.jetposemovies.movies.data.models.local.PopularMoviesEntity
+import br.com.lucasisrael.jetposemovies.movies.data.models.local.UpcomingMoviesEntity
 
 @Database(
-    entities = [MovieEntity::class],
+    entities = [MovieEntity::class, UpcomingMoviesEntity::class, PopularMoviesEntity::class],
     version = 1,
     exportSchema = false
 )
 @TypeConverters(
     ListConverters::class
 )
-abstract class MoviesDataBase : RoomDatabase() {
-
-    abstract fun movieDao(): MoviesDao
-
+abstract class MoviesDataBase: RoomDatabase() {
+    abstract val movieDao: MovieDao
+    abstract val upcomingDao: UpcomingMoviesDao
+    abstract val popularDao: PopularMoviesDao
 }
