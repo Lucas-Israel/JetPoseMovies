3180e0476cb419ff9c7d61f504127c0e59a23468
Lucas Israel <lisraelalves92@gmail.com>
2024-12-18 13:16:50 -0300

    added toprateddao in MoviesDataBase

--- app/src/main/java/br/com/lucasisrael/jetposemovies/movies/data/datasource/local/MoviesDataBase.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/movies/data/datasource/local/MoviesDataBase.kt
@@ -6,18 +6,25 @@ import androidx.room.TypeConverters
 import br.com.lucasisrael.jetposemovies.common.converters.ListConverters
 import br.com.lucasisrael.jetposemovies.movies.data.models.local.MovieEntity
 import br.com.lucasisrael.jetposemovies.movies.data.models.local.PopularMoviesEntity
+import br.com.lucasisrael.jetposemovies.movies.data.models.local.TopRatedMoviesEntity
 import br.com.lucasisrael.jetposemovies.movies.data.models.local.UpcomingMoviesEntity
 
 @Database(
-    entities = [MovieEntity::class, UpcomingMoviesEntity::class, PopularMoviesEntity::class],
+    entities = [
+        MovieEntity::class,
+        UpcomingMoviesEntity::class,
+        PopularMoviesEntity::class,
+        TopRatedMoviesEntity::class,
+    ],
     version = 1,
     exportSchema = false
 )
 @TypeConverters(
     ListConverters::class
 )
-abstract class MoviesDataBase: RoomDatabase() {
+abstract class MoviesDataBase : RoomDatabase() {
     abstract val movieDao: MovieDao
     abstract val upcomingDao: UpcomingMoviesDao
     abstract val popularDao: PopularMoviesDao
+    abstract val topRatedDao: TopRatedDao
 }
