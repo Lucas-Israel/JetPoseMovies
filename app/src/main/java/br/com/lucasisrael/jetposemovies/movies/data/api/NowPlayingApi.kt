944bbfbd60469ba44b1fc539d5135ccefa7cafc5
Lucas Israel <lisraelalves92@gmail.com>
2024-12-20 11:00:47 -0300

    added NowPlayingApi

---
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/movies/data/api/NowPlayingApi.kt
@@ -0,0 +1,13 @@
+package br.com.lucasisrael.jetposemovies.movies.data.api
+
+import br.com.lucasisrael.jetposemovies.movies.data.models.response.MoviesResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NowPlayingApi {
+    @GET("movie/now_playing")
+    suspend fun fetch(
+        @Query("language") language: String? = "en-US",
+        @Query("page") page: Int? = 1
+    ): MoviesResponse
+}
