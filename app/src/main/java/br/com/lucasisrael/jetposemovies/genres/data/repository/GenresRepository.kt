a814fe84d689fc7989f27d08c8cbeaa8645a33fb
Lucas Israel <lisraelalves92@gmail.com>
2024-12-27 11:07:56 -0300

    GenresRepository now returns Flow<PagingData>

--- app/src/main/java/br/com/lucasisrael/jetposemovies/genres/data/repository/GenresRepository.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/genres/data/repository/GenresRepository.kt
@@ -1,14 +1,35 @@
 package br.com.lucasisrael.jetposemovies.genres.data.repository
 
-import androidx.paging.PagingSource
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.map
+import br.com.lucasisrael.jetposemovies.genres.data.api.GenresApi
 import br.com.lucasisrael.jetposemovies.genres.data.datasource.local.GenresDao
-import br.com.lucasisrael.jetposemovies.genres.data.models.local.GenreEntity
+import br.com.lucasisrael.jetposemovies.genres.data.datasource.remote.GenresRemoteMediator
+import br.com.lucasisrael.jetposemovies.genres.data.mappers.toDomain
+import br.com.lucasisrael.jetposemovies.genres.models.domain.GenreDomain
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class GenresRepository @Inject constructor(
     private val dao: GenresDao,
+    private val api: GenresApi,
 ) {
-    fun load(): PagingSource<Int, GenreEntity> {
-        return dao.load()
+    @OptIn(ExperimentalPagingApi::class)
+    fun flow(): Flow<PagingData<GenreDomain>> {
+        val remoteMediator = GenresRemoteMediator(dao = dao, api = api)
+
+        return Pager(
+            config = PagingConfig(pageSize = 20),
+            remoteMediator = remoteMediator,
+            pagingSourceFactory = { dao.load() }
+        ).flow
+            .map { pagingData -> pagingData.map { it.toDomain() } }
+            .flowOn(Dispatchers.IO)
     }
 }
