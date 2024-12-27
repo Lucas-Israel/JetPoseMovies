86ec6b5b2f746d84586517a1c70efd01dbf30575
Lucas Israel <lisraelalves92@gmail.com>
2024-12-27 15:45:27 -0300

    renamed table name from details_structure to details and nullable values

--- app/src/main/java/br/com/lucasisrael/jetposemovies/details/models/local/DetailsEntity.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/details/models/local/DetailsEntity.kt
@@ -1,4 +1,4 @@
-package br.com.lucasisrael.jetposemovies.details.data.models.local
+package br.com.lucasisrael.jetposemovies.details.models.local
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
@@ -15,7 +15,7 @@ import br.com.lucasisrael.jetposemovies.common.converters.ProductionCompanyConve
 import br.com.lucasisrael.jetposemovies.common.converters.SpokenLanguagesConverters
 import br.com.lucasisrael.jetposemovies.genres.models.local.GenreEntity
 
-@Entity(tableName = "details_structure")
+@Entity(tableName = "details")
 @TypeConverters(
     MovieCollectionConverters::class,
     GenreConverters::class,
@@ -24,44 +24,44 @@ import br.com.lucasisrael.jetposemovies.genres.models.local.GenreEntity
     SpokenLanguagesConverters::class,
 )
 data class DetailsEntity(
-    @PrimaryKey(autoGenerate = true) val detailsId: Int = 0,
-    val adult: Boolean,
+    @PrimaryKey(autoGenerate = true) val detailsId: Int? = 0,
+    val adult: Boolean?,
     @ColumnInfo(name = "backdrop_path")
-    val backdropPath: String,
+    val backdropPath: String?,
     @ColumnInfo(name = "belongs_to_collection")
-    val belongsToCollection: MovieCollection,
-    val budget: Int,
-    val genres: List<GenreEntity>,
-    val homepage: String,
-    val id: Int,
+    val belongsToCollection: MovieCollection?,
+    val budget: Int?,
+    val genres: List<GenreEntity>?,
+    val homepage: String?,
+    val id: Int?,
     @ColumnInfo(name = "imdb_id")
-    val imdbId: String,
+    val imdbId: String?,
     @ColumnInfo(name = "origin_country")
-    val originCountry: List<String>,
+    val originCountry: List<String>?,
     @ColumnInfo(name = "original_language")
-    val originalLanguage: String,
+    val originalLanguage: String?,
     @ColumnInfo(name = "original_title")
-    val originalTitle: String,
-    val overview: String,
-    val popularity: Double,
+    val originalTitle: String?,
+    val overview: String?,
+    val popularity: Double?,
     @ColumnInfo(name = "poster_path")
-    val posterPath: String,
+    val posterPath: String?,
     @ColumnInfo(name = "production_companies")
-    val productionCompanies: List<ProductionCompany>,
+    val productionCompanies: List<ProductionCompany>?,
     @ColumnInfo(name = "production_countries")
-    val productionCountries: List<ISOCountry>,
+    val productionCountries: List<ISOCountry>?,
     @ColumnInfo(name = "release_date")
-    val releaseDate: String,
-    val revenue: Long,
-    val runtime: Int,
+    val releaseDate: String?,
+    val revenue: Long?,
+    val runtime: Int?,
     @ColumnInfo(name = "spoken_languages")
-    val spokenLanguages: List<SpokenLanguages>,
-    val status: String,
-    val tagline: String,
-    val title: String,
-    val video: Boolean,
+    val spokenLanguages: List<SpokenLanguages>?,
+    val status: String?,
+    val tagline: String?,
+    val title: String?,
+    val video: Boolean?,
     @ColumnInfo(name = "vote_average")
-    val voteAverage: Double,
+    val voteAverage: Double?,
     @ColumnInfo(name = "vote_count")
-    val voteCount: Int,
+    val voteCount: Int?,
 )
