4c5cf99b66ab373d87d0e5c855c35c06c3c6bd59
Lucas Israel <lisraelalves92@gmail.com>
2024-12-27 10:41:31 -0300

    repositories now returns Flow<PagingData>

--- app/src/main/java/br/com/lucasisrael/jetposemovies/movies/data/repository/NowPlayingMoviesRepository.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/movies/data/repository/NowPlayingMoviesRepository.kt
@@ -1,14 +1,35 @@
 package br.com.lucasisrael.jetposemovies.movies.data.repository
 
-import androidx.paging.PagingSource
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.map
+import br.com.lucasisrael.jetposemovies.movies.data.api.NowPlayingApi
 import br.com.lucasisrael.jetposemovies.movies.data.datasource.local.NowPlayingDao
-import br.com.lucasisrael.jetposemovies.movies.data.models.local.NowPlayingWithMovie
+import br.com.lucasisrael.jetposemovies.movies.data.datasource.remote.NowPlayingMoviesRemoteMediator
+import br.com.lucasisrael.jetposemovies.movies.data.mappers.toDomain
+import br.com.lucasisrael.jetposemovies.movies.models.domain.MovieDomain
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
+@OptIn(ExperimentalPagingApi::class)
 class NowPlayingMoviesRepository @Inject constructor(
     private val dao: NowPlayingDao,
+    private val api: NowPlayingApi,
 ) {
-    fun load(): PagingSource<Int, NowPlayingWithMovie> {
-        return dao.load()
+    fun flow(): Flow<PagingData<MovieDomain>> {
+        val remoteMediator = NowPlayingMoviesRemoteMediator(dao = dao, api = api)
+
+        return Pager(
+            config = PagingConfig(pageSize = 20),
+            remoteMediator = remoteMediator,
+            pagingSourceFactory = { dao.load() }
+        ).flow
+            .map { pagingData -> pagingData.map { it.movie.toDomain() } }
+            .flowOn(Dispatchers.IO)
     }
 }
