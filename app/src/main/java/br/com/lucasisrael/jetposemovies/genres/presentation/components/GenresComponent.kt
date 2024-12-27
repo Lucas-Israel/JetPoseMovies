dd2f6758847a4f4cd9914af7a3b85bcd4ea21e1a
Lucas Israel <lisraelalves92@gmail.com>
2024-12-27 10:28:12 -0300

    using items in GenresComponent

--- app/src/main/java/br/com/lucasisrael/jetposemovies/genres/presentation/components/GenresComponent.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/genres/presentation/components/GenresComponent.kt
@@ -10,13 +10,12 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.paging.compose.collectAsLazyPagingItems
 import br.com.lucasisrael.jetposemovies.common.navigation.NavigationActions
 import br.com.lucasisrael.jetposemovies.genres.presentation.viewmodel.GenresViewModel
 import kotlinx.serialization.Serializable
@@ -31,41 +30,40 @@ fun GenresComponent(
     viewModel: GenresViewModel = hiltViewModel(),
 ) {
 
-    val collectingGenres by viewModel.genres.collectAsStateWithLifecycle()
+    val data = viewModel.pagingFlow.collectAsLazyPagingItems()
 
     LazyRow(
         modifier = Modifier
             .padding(8.dp)
     ) {
-        if (collectingGenres.isNotEmpty()) {
-            collectingGenres.forEach {
-                item {
-                    Box(
-                        modifier = Modifier
-                            .padding(4.dp)
-                            .clickable {
-                                navigationActions.toMovieGenreScreen(
-                                    it.id.toString(),
-                                    it.name
-                                )
-                            }
-                    ) {
-                        Box(
-                            contentAlignment = Alignment.Center,
-                            modifier = Modifier
-                                .clip(RoundedCornerShape(6.dp))
-                                .background(MaterialTheme.colorScheme.surfaceContainerHigh)
-                                .padding(12.dp)
-                                .width(150.dp)
-                        ) {
-                            Text(
-                                text = it.name,
-                                modifier = Modifier
+        items(count = data.itemCount) { index ->
+            val genre = data[index]
+            if (genre != null) {
+                Box(
+                    modifier = Modifier
+                        .padding(4.dp)
+                        .clickable {
+                            navigationActions.toMovieGenreScreen(
+                                genre.id,
+                                genre.name
                             )
-
                         }
+                ) {
+                    Box(
+                        contentAlignment = Alignment.Center,
+                        modifier = Modifier
+                            .clip(RoundedCornerShape(6.dp))
+                            .background(MaterialTheme.colorScheme.surfaceContainerHigh)
+                            .padding(12.dp)
+                            .width(150.dp)
+                    ) {
+                        Text(
+                            text = genre.name,
+                            modifier = Modifier
+                        )
 
                     }
+
                 }
             }
         }
