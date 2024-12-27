e9b26596540e59245f9cae65c59a5779a45712c3
Lucas Israel <lisraelalves92@gmail.com>
2024-12-27 11:07:07 -0300

    added @Transaction to GenresDao

--- app/src/main/java/br/com/lucasisrael/jetposemovies/genres/data/datasource/local/GenresDao.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/genres/data/datasource/local/GenresDao.kt
@@ -3,18 +3,22 @@ package br.com.lucasisrael.jetposemovies.genres.data.datasource.local
 import androidx.paging.PagingSource
 import androidx.room.Dao
 import androidx.room.Query
+import androidx.room.Transaction
 import androidx.room.Upsert
-import br.com.lucasisrael.jetposemovies.genres.data.models.local.GenreEntity
+import br.com.lucasisrael.jetposemovies.genres.models.local.GenreEntity
 
 @Dao
 interface GenresDao {
 
     @Upsert
+    @Transaction
     fun upsert(list: List<GenreEntity>)
 
     @Query("SELECT * from genres")
+    @Transaction
     fun load(): PagingSource<Int, GenreEntity>
 
     @Query("DELETE from genres")
+    @Transaction
     fun clearAll()
 }
