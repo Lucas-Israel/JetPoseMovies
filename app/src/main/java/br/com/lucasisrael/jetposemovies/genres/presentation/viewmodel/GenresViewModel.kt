24051f5a5d22c8113849d9e8a515ce330c399896
Lucas Israel <lisraelalves92@gmail.com>
2024-12-27 10:29:53 -0300

    simplification of GenresViewModel

--- app/src/main/java/br/com/lucasisrael/jetposemovies/genres/presentation/viewmodel/GenresViewModel.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/genres/presentation/viewmodel/GenresViewModel.kt
@@ -2,39 +2,17 @@ package br.com.lucasisrael.jetposemovies.genres.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import br.com.lucasisrael.jetposemovies.common.coroutines.CoroutinesProvider
-import br.com.lucasisrael.jetposemovies.genres.domain.models.Genre
-import br.com.lucasisrael.jetposemovies.genres.domain.usecase.LoadGenresUseCase
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import br.com.lucasisrael.jetposemovies.genres.domain.models.GenreDomain
+import br.com.lucasisrael.jetposemovies.genres.domain.usecase.GenresUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.CancellationException
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 @HiltViewModel
 class GenresViewModel @Inject constructor(
-    private val genresUseCase: LoadGenresUseCase,
-    private val coroutinesProvider: CoroutinesProvider
+    useCase: GenresUseCase,
 ) : ViewModel() {
-
-    private val _genres =
-        MutableStateFlow<List<Genre>>(listOf())
-    val genres: StateFlow<List<Genre>> = _genres.asStateFlow()
-
-    init {
-        loadGenres()
-    }
-
-    private fun loadGenres() {
-        viewModelScope.launch(coroutinesProvider.io()) {
-            try {
-                _genres.value = genresUseCase.getGenres()
-
-            } catch (e: CancellationException) {
-                e.printStackTrace()
-            }
-        }
-    }
+    var pagingFlow: Flow<PagingData<GenreDomain>> = useCase.flow().cachedIn(viewModelScope)
 }
