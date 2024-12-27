d766a918a529d8967da1fca2a89f6464f9bc35f9
Lucas Israel <lisraelalves92@gmail.com>
2024-12-27 10:33:18 -0300

    changing parameter from usecase to repository

--- app/src/main/java/br/com/lucasisrael/jetposemovies/movies/presentation/viewmodel/MovieListViewModel.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/movies/presentation/viewmodel/MovieListViewModel.kt
@@ -4,23 +4,21 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
-import br.com.lucasisrael.jetposemovies.movies.domain.models.MovieDomain
-import br.com.lucasisrael.jetposemovies.movies.domain.usecase.MoviesUseCase
+import br.com.lucasisrael.jetposemovies.movies.data.repository.MoviesRepository
+import br.com.lucasisrael.jetposemovies.movies.models.domain.MovieDomain
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.emptyFlow
-
 import javax.inject.Inject
-import kotlin.jvm.Throws
 
 @HiltViewModel
 class MovieListViewModel @Inject constructor(
-    private val moviesUseCase: MoviesUseCase,
+    private val moviesRepository: MoviesRepository,
 ) : ViewModel() {
 
     var pagingFlow: Flow<PagingData<MovieDomain>> = emptyFlow()
 
-    fun setFlow(genreId: String) {
-        pagingFlow = moviesUseCase.flow(genreId).cachedIn(viewModelScope)
+    fun setFlow(genreId: Int) {
+        pagingFlow = moviesRepository.flow(genreId).cachedIn(viewModelScope)
     }
 }
