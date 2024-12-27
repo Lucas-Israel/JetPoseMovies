34f0a0a1eff24c20acdec33647981199c36b31b1
Lucas Israel <lisraelalves92@gmail.com>
2024-12-27 15:39:22 -0300

    changed return from DetailsResponse to DetailsDto

--- app/src/main/java/br/com/lucasisrael/jetposemovies/details/data/api/DetailsApi.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/details/data/api/DetailsApi.kt
@@ -1,10 +1,12 @@
 package br.com.lucasisrael.jetposemovies.details.data.api
 
-import br.com.lucasisrael.jetposemovies.details.data.models.response.DetailsResponse
+import br.com.lucasisrael.jetposemovies.details.models.remote.DetailsDto
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface DetailsApi {
     @GET("movie/{movie_id}")
-    suspend fun getDetailsById(@Path("movie_id") movieId: String) : DetailsResponse
+    suspend fun fetch(
+        @Path("movie_id") movieId: Int,
+    ): DetailsDto
 }
