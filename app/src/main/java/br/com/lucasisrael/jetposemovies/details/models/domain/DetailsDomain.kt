d119838f11589123343a9df96d551a2b07682836
Lucas Israel <lisraelalves92@gmail.com>
2024-12-27 15:46:10 -0300

    added DetailsDomain

---
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/details/models/domain/DetailsDomain.kt
@@ -0,0 +1,37 @@
+package br.com.lucasisrael.jetposemovies.details.models.domain
+
+import br.com.lucasisrael.jetposemovies.common.models.ISOCountry
+import br.com.lucasisrael.jetposemovies.common.models.MovieCollection
+import br.com.lucasisrael.jetposemovies.common.models.ProductionCompany
+import br.com.lucasisrael.jetposemovies.common.models.SpokenLanguages
+import br.com.lucasisrael.jetposemovies.genres.models.domain.GenreDomain
+
+data class DetailsDomain (
+    val detailsId: Int?,
+    val adult: Boolean?,
+    val backdropPath: String?,
+    val belongsToCollection: MovieCollection?,
+    val budget: Int?,
+    val genres: List<GenreDomain>?,
+    val homepage: String?,
+    val id: Int?,
+    val imdbId: String?,
+    val originCountry: List<String>?,
+    val originalLanguage: String?,
+    val originalTitle: String?,
+    val overview: String?,
+    val popularity: Double?,
+    val posterPath: String?,
+    val productionCompanies: List<ProductionCompany>?,
+    val productionCountries: List<ISOCountry>?,
+    val releaseDate: String?,
+    val revenue: Long?,
+    val runtime: Int?,
+    val spokenLanguages: List<SpokenLanguages>?,
+    val status: String?,
+    val tagline: String?,
+    val title: String?,
+    val video: Boolean?,
+    val voteAverage: Double?,
+    val voteCount: Int?,
+)
