ab288771aea0f83f2a017d99e3f22b50af54ae3b
Lucas Israel <lisraelalves92@gmail.com>
2024-12-27 15:47:45 -0300

    to be removed DetailConverters

--- app/src/main/java/br/com/lucasisrael/jetposemovies/common/converters/DetailConverters.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/common/converters/DetailConverters.kt
@@ -1,7 +1,7 @@
 package br.com.lucasisrael.jetposemovies.common.converters
 
 import androidx.room.TypeConverter
-import br.com.lucasisrael.jetposemovies.details.data.models.local.DetailsEntity
+import br.com.lucasisrael.jetposemovies.details.models.local.DetailsEntity
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 
