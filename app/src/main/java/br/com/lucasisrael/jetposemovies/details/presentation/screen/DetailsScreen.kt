c43308f96dcab311b9f280c207d60b6525492bb7
Lucas Israel <lisraelalves92@gmail.com>
2024-12-27 15:44:09 -0300

    DetailsScreen using lazy paging items

--- app/src/main/java/br/com/lucasisrael/jetposemovies/details/presentation/screen/DetailsScreen.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/details/presentation/screen/DetailsScreen.kt
@@ -1,21 +1,27 @@
 package br.com.lucasisrael.jetposemovies.details.presentation.screen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.paging.LoadState
+import androidx.paging.compose.collectAsLazyPagingItems
 import br.com.lucasisrael.jetposemovies.common.navigation.NavigationActions
 import br.com.lucasisrael.jetposemovies.common.presentation.screens.LoadingScreen
 import br.com.lucasisrael.jetposemovies.common.presentation.screens.ScreenStructure
 import br.com.lucasisrael.jetposemovies.details.presentation.viewmodel.DetailsViewModel
-import br.com.lucasisrael.jetposemovies.details.presentation.components.DetailCard
+import br.com.lucasisrael.jetposemovies.details.presentation.components.DetailItem
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class DetailsScreen(
-    val movieId: String
+    val movieId: Int,
 )
 
 @Suppress("FunctionNaming")
@@ -23,27 +29,38 @@ data class DetailsScreen(
 fun DetailsScreen(
     navigationActions: NavigationActions,
     viewModel: DetailsViewModel = hiltViewModel(),
-    movieId: String
+    movieId: Int,
 ) {
-    LaunchedEffect(Unit) {
-        viewModel.getFromRepository(movieId)
+    val rememberedMovieId = remember {
+        movieId
     }
 
-    val collectingDetails by viewModel.details.collectAsStateWithLifecycle()
-    val isLoading by viewModel.isLoading.collectAsStateWithLifecycle()
+    LaunchedEffect(rememberedMovieId) {
+        viewModel.setFlow(movieId = rememberedMovieId)
+    }
 
+    val details = viewModel.pagingFlow.collectAsLazyPagingItems()
 
-    if (isLoading) {
+    if (details.loadState.refresh is LoadState.Loading) {
         LoadingScreen()
     } else {
         ScreenStructure {
-
-            DetailCard(
-                navigationActions = navigationActions,
-                modifier = Modifier,
-                details = collectingDetails,
-            )
-
+            LazyColumn(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.spacedBy(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                items(count = details.itemCount) { index ->
+                    val detail = details[index]
+                    if (detail != null) {
+                        DetailItem(
+                            navigationActions = navigationActions,
+                            modifier = Modifier,
+                            details = detail,
+                        )
+                    }
+                }
+            }
         }
     }
 }
