797aca6e29235f1cfed8a06245cdcfe4625033c6
Lucas Israel <lisraelalves92@gmail.com>
2024-12-27 11:08:10 -0300

    GenresViewModel now used repository

--- app/src/main/java/br/com/lucasisrael/jetposemovies/genres/presentation/viewmodel/GenresViewModel.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/genres/presentation/viewmodel/GenresViewModel.kt
@@ -4,15 +4,15 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
-import br.com.lucasisrael.jetposemovies.genres.domain.models.GenreDomain
-import br.com.lucasisrael.jetposemovies.genres.domain.usecase.GenresUseCase
+import br.com.lucasisrael.jetposemovies.genres.data.repository.GenresRepository
+import br.com.lucasisrael.jetposemovies.genres.models.domain.GenreDomain
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 @HiltViewModel
 class GenresViewModel @Inject constructor(
-    useCase: GenresUseCase,
+    repository: GenresRepository,
 ) : ViewModel() {
-    var pagingFlow: Flow<PagingData<GenreDomain>> = useCase.flow().cachedIn(viewModelScope)
+    var pagingFlow: Flow<PagingData<GenreDomain>> = repository.flow().cachedIn(viewModelScope)
 }
