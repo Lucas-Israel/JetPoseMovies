1cb63d449b5da3c0db71910062e8a0d3691c51c6
Lucas Israel <lisraelalves92@gmail.com>
2024-12-27 15:46:57 -0300

    better mapping for DetailsMappers.kt

--- app/src/main/java/br/com/lucasisrael/jetposemovies/details/data/mappers/DetailsMappers.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/details/data/mappers/DetailsMappers.kt
@@ -1,104 +1,71 @@
 package br.com.lucasisrael.jetposemovies.details.data.mappers
 
-import br.com.lucasisrael.jetposemovies.common.utils.types.values.nullable.replaceNullable
-import br.com.lucasisrael.jetposemovies.details.domain.models.Details
-import br.com.lucasisrael.jetposemovies.details.data.models.local.DetailsEntity
-import br.com.lucasisrael.jetposemovies.details.data.models.remote.DetailsDto
-import br.com.lucasisrael.jetposemovies.details.data.models.response.DetailsResponse
+import br.com.lucasisrael.jetposemovies.details.models.domain.DetailsDomain
+import br.com.lucasisrael.jetposemovies.details.models.local.DetailsEntity
+import br.com.lucasisrael.jetposemovies.details.models.remote.DetailsDto
 import br.com.lucasisrael.jetposemovies.genres.data.mappers.toDomain
 import br.com.lucasisrael.jetposemovies.genres.data.mappers.toEntity
-
-
-fun DetailsResponse.toDetailsDto(): DetailsDto {
-    return DetailsDto(
-        adult.replaceNullable(),
-        backdropPath.replaceNullable(),
-        belongsToCollection.replaceNullable(),
-        budget.replaceNullable(),
-        genres.replaceNullable(),
-        homepage.replaceNullable(),
-        id.replaceNullable(),
-        imdbId.replaceNullable(),
-        originCountry.replaceNullable(),
-        originalLanguage.replaceNullable(),
-        originalTitle.replaceNullable(),
-        overview.replaceNullable(),
-        popularity.replaceNullable(),
-        posterPath.replaceNullable(),
-        productionCompanies.replaceNullable(),
-        productionCountries.replaceNullable(),
-        releaseDate.replaceNullable(),
-        revenue.replaceNullable(),
-        runtime.replaceNullable(),
-        spokenLanguages.replaceNullable(),
-        status.replaceNullable(),
-        tagline.replaceNullable(),
-        title.replaceNullable(),
-        video.replaceNullable(),
-        voteAverage.replaceNullable(),
-        voteCount.replaceNullable()
-    )
-}
 
 fun DetailsDto.toDetailsEntity(): DetailsEntity {
     return DetailsEntity(
         detailsId = 0,
-        adult,
-        backdropPath,
-        belongsToCollection,
-        budget,
-        genres = genres.map { it.toEntity() },
-        homepage,
-        id,
-        imdbId,
-        originCountry,
-        originalLanguage,
-        originalTitle,
-        overview,
-        popularity,
-        posterPath,
-        productionCompanies,
-        productionCountries,
-        releaseDate,
-        revenue,
-        runtime,
-        spokenLanguages,
-        status,
-        tagline,
-        title,
-        video,
-        voteAverage,
-        voteCount
+        adult = adult,
+        backdropPath = backdropPath,
+        belongsToCollection = belongsToCollection,
+        budget = budget,
+        genres = genres?.map { it.toEntity() },
+        homepage = homepage,
+        id = id,
+        imdbId = imdbId,
+        originCountry = originCountry,
+        originalLanguage = originalLanguage,
+        originalTitle = originalTitle,
+        overview = overview,
+        popularity = popularity,
+        posterPath = posterPath,
+        productionCompanies = productionCompanies,
+        productionCountries = productionCountries,
+        releaseDate = releaseDate,
+        revenue = revenue,
+        runtime = runtime,
+        spokenLanguages = spokenLanguages,
+        status = status,
+        tagline = tagline,
+        title = title,
+        video = video,
+        voteAverage = voteAverage,
+        voteCount = voteCount
     )
 }
 
-fun DetailsEntity.toDetails(): Details {
-    return Details(
-        adult,
-        backdropPath,
-        belongsToCollection,
-        budget,
-        genreDomains = genres.map { it.toDomain() },
-        homepage,
-        id,
-        imdbId,
-        originCountry,
-        originalLanguage,
-        originalTitle,
-        overview,
-        popularity,
-        posterPath,
-        productionCompanies,
-        productionCountries,
-        releaseDate,
-        revenue,
-        runtime,
-        spokenLanguages,
-        status,
-        tagline,
-        title,
-        video,
-        voteAverage,
-        voteCount
+fun DetailsEntity.toDomain(): DetailsDomain {
+    return DetailsDomain(
+        detailsId = 0,
+        adult = adult,
+        backdropPath = backdropPath,
+        belongsToCollection = belongsToCollection,
+        budget = budget,
+        genres = genres?.map { it.toDomain() },
+        homepage = homepage,
+        id = id,
+        imdbId = imdbId,
+        originCountry = originCountry,
+        originalLanguage = originalLanguage,
+        originalTitle = originalTitle,
+        overview = overview,
+        popularity = popularity,
+        posterPath = posterPath,
+        productionCompanies = productionCompanies,
+        productionCountries = productionCountries,
+        releaseDate = releaseDate,
+        revenue = revenue,
+        runtime = runtime,
+        spokenLanguages = spokenLanguages,
+        status = status,
+        tagline = tagline,
+        title = title,
+        video = video,
+        voteAverage = voteAverage,
+        voteCount = voteCount
     )
 }
