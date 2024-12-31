ff38237ea654cdab5a7929da19820b84694c37ba
Lucas Israel <lisraelalves92@gmail.com>
2024-12-31 13:27:39 -0300

    removed unnecessary files

--- app/src/main/java/br/com/lucasisrael/jetposemovies/details/models/local/ISOCountry.kt
+++
@@ -1,9 +0,0 @@
-package br.com.lucasisrael.jetposemovies.details.models.local
-
-import com.google.gson.annotations.SerializedName
-
-data class ISOCountry(
-    @SerializedName("iso_3166_1")
-    val iso3166u1: String,
-    val name: String,
-)
