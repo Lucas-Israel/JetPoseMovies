19392a7c6f094230526272185fb1c2ff39532ce5
Lucas Israel <lisraelalves92@gmail.com>
2024-12-31 18:22:58 -0300

    fixed type in converter

--- app/src/main/java/br/com/lucasisrael/jetposemovies/common/converters/Converters.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/common/converters/Converters.kt
@@ -1,20 +1,20 @@
 package br.com.lucasisrael.jetposemovies.common.converters
 
 import androidx.room.TypeConverter
-import br.com.lucasisrael.jetposemovies.genres.models.remote.GenreDto
+import br.com.lucasisrael.jetposemovies.genres.models.local.GenreEntity
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 
 class Converters {
 
     @TypeConverter
-    fun fromGenres(value: String): List<GenreDto> {
-        val listType = object : TypeToken<List<GenreDto>>() {}.type
+    fun fromGenres(value: String): List<GenreEntity> {
+        val listType = object : TypeToken<List<GenreEntity>>() {}.type
         return Gson().fromJson(value, listType)
     }
 
     @TypeConverter
-    fun fromList(list: List<GenreDto>): String {
+    fun fromList(list: List<GenreEntity>): String {
         return Gson().toJson(list)
     }
 
