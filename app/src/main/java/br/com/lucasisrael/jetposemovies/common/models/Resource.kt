387d8957a396dffde2c7249387516714b55a6ac8
Lucas Israel <lisraelalves92@gmail.com>
2025-01-07 21:05:02 -0300

    removed

--- app/src/main/java/br/com/lucasisrael/jetposemovies/common/models/Resource.kt
+++
@@ -1,8 +0,0 @@
-package br.com.lucasisrael.jetposemovies.common.models
-
-sealed class Resource<T>(val data: T?, val message: String?) {
-
-    class Success<T>(data: T, message: String?): Resource<T>(data, message)
-    class Error<T>(data: T?, message: String): Resource<T>(data, message)
-
-}
