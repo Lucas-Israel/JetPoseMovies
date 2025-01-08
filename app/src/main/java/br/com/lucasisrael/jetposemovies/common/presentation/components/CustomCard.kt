2b3b2d71dcfcc77e9b1d1f1171a9443e30520d18
Lucas Israel <lisraelalves92@gmail.com>
2025-01-07 21:08:14 -0300

    removed unnecessary box

--- app/src/main/java/br/com/lucasisrael/jetposemovies/common/presentation/components/CustomCard.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/common/presentation/components/CustomCard.kt
@@ -1,13 +1,8 @@
 package br.com.lucasisrael.jetposemovies.common.presentation.components
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -19,25 +14,16 @@ fun CustomCard(
     url: String? = null,
     modifier: Modifier,
 ) {
-
-    Box(
-        contentAlignment = Alignment.Center, modifier = modifier
-            .fillMaxSize()
-            .padding(4.dp)
+    Card(
+        elevation = CardDefaults.cardElevation(10.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.DarkGray
+        )
     ) {
-
-        Card(
-            modifier = Modifier.fillMaxWidth(),
-            elevation = CardDefaults.cardElevation(10.dp),
-            colors = CardDefaults.cardColors(
-                containerColor = Color.DarkGray
-            )
-        ) {
-            CustomAsyncImage(
-                url = url,
-                title = title,
-                modifier = modifier,
-            )
-        }
+        CustomAsyncImage(
+            url = url,
+            title = title,
+            modifier = modifier,
+        )
     }
 }
