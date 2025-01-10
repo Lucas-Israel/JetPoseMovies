54c6bb032d943d835292c7730ef48590fc3cb2f8
Lucas Israel <lisraelalves92@gmail.com>
2025-01-09 22:24:01 -0300

    added modifier to HomepageUrl and removed spacer

--- app/src/main/java/br/com/lucasisrael/jetposemovies/details/presentation/components/HomepageUrl.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/details/presentation/components/HomepageUrl.kt
@@ -3,31 +3,29 @@ package br.com.lucasisrael.jetposemovies.details.presentation.components
 import android.content.Intent
 import android.net.Uri
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.res.stringResource
+import br.com.lucasisrael.jetposemovies.R
 
 @SuppressWarnings("FunctionNaming")
 @Composable
 fun HomepageUrl(
-    homePageUrl: String,
+    homePageUrl: String?,
+    modifier: Modifier
 ) {
     val context = LocalContext.current
 
     Text(
-        text = homePageUrl,
-        modifier = Modifier
+        text = stringResource(R.string.visit_website),
+        color = MaterialTheme.colorScheme.primary,
+        modifier = modifier
             .clickable {
                 val intent = Intent(Intent.ACTION_VIEW, Uri.parse(homePageUrl))
                 context.startActivity(intent)
             }
-    )
-
-    Spacer(
-        modifier = Modifier.padding(bottom = 16.dp)
     )
 }
