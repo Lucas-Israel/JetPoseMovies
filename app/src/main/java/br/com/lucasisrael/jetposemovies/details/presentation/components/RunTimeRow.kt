d89e9305eb348beac4e94d8dcf2c75c705efff41
Lucas Israel <lisraelalves92@gmail.com>
2025-01-09 22:36:35 -0300

    added condition to RunTimeRow

--- app/src/main/java/br/com/lucasisrael/jetposemovies/details/presentation/components/RunTimeRow.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/details/presentation/components/RunTimeRow.kt
@@ -1,24 +1,26 @@
 package br.com.lucasisrael.jetposemovies.details.presentation.components
 
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import br.com.lucasisrael.jetposemovies.R
 import br.com.lucasisrael.jetposemovies.common.utils.time.fromMinutesToHours
 
 @SuppressWarnings("FunctionNaming")
 @Composable
 fun RunTimeRow(
-    runtime: Int,
-    modifier: Modifier
+    runtime: Int?,
+    modifier: Modifier,
 ) {
+    val runTimeText = if (runtime == null) {
+        stringResource(R.string.field_not_available)
+    } else {
+        runtime.fromMinutesToHours().toString() + "h"
+    }
 
     Row(
         modifier = modifier
@@ -27,13 +29,8 @@ fun RunTimeRow(
             painter = painterResource(id = R.drawable.icons8_clock_64),
             contentDescription = stringResource(R.string.clock_icon)
         )
-
-        Spacer(modifier = Modifier.padding(8.dp))
-
         Text(
-            text = runtime.fromMinutesToHours().toString() + "h"
+            text = runTimeText
         )
-
     }
-
 }
