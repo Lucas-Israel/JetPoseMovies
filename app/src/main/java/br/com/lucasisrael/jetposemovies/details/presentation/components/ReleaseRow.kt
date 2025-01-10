b867697eb2334246d6541175540939f839850654
Lucas Israel <lisraelalves92@gmail.com>
2025-01-09 22:28:29 -0300

    added stringResource to ReleaseRow in case releaseDate is null

--- app/src/main/java/br/com/lucasisrael/jetposemovies/details/presentation/components/ReleaseRow.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/details/presentation/components/ReleaseRow.kt
@@ -1,28 +1,26 @@
 package br.com.lucasisrael.jetposemovies.details.presentation.components
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import br.com.lucasisrael.jetposemovies.R
 
 @SuppressWarnings("FunctionNaming")
 @Composable
 fun ReleaseRow(
-    releaseDate: String,
+    releaseDate: String?,
     modifier: Modifier
 ) {
     Row(
         modifier = modifier
     ) {
         Text(
-            text = stringResource(R.string.release_date, releaseDate),
+            text = stringResource(
+                R.string.release_date,
+                releaseDate ?: stringResource(R.string.field_not_available)
+            ),
         )
     }
 
