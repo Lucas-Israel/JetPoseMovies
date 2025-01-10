baffd0b024f30f2e26ebcca641e6b667689bea0d
Lucas Israel <lisraelalves92@gmail.com>
2025-01-09 22:22:38 -0300

    removed unnecessary parameters in DetailsScreen

--- app/src/main/java/br/com/lucasisrael/jetposemovies/details/presentation/screen/DetailsScreen.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/details/presentation/screen/DetailsScreen.kt
@@ -1,7 +1,8 @@
 package br.com.lucasisrael.jetposemovies.details.presentation.screen
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -10,10 +11,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.paging.LoadState
 import androidx.paging.compose.collectAsLazyPagingItems
 import br.com.lucasisrael.jetposemovies.common.navigation.NavigationActions
-import br.com.lucasisrael.jetposemovies.common.presentation.screens.LoadingScreen
 import br.com.lucasisrael.jetposemovies.common.presentation.screens.ScreenStructure
 import br.com.lucasisrael.jetposemovies.details.presentation.viewmodel.DetailsViewModel
 import br.com.lucasisrael.jetposemovies.details.presentation.components.DetailItem
@@ -41,26 +40,23 @@ fun DetailsScreen(
 
     val details = viewModel.pagingFlow.collectAsLazyPagingItems()
 
-    if (details.loadState.refresh is LoadState.Loading) {
-        LoadingScreen()
-    } else {
-        ScreenStructure {
-            LazyColumn(
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.spacedBy(16.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                items(count = details.itemCount) { index ->
-                    val detail = details[index]
-                    if (detail != null) {
-                        DetailItem(
-                            navigationActions = navigationActions,
-                            modifier = Modifier,
-                            details = detail,
-                        )
-                    }
+    ScreenStructure {
+        LazyColumn(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(start = 8.dp, end = 8.dp)
+        ) {
+            items(count = details.itemCount) { index ->
+                val detail = details[index]
+                if (detail != null) {
+                    DetailItem(
+                        details = detail,
+                        navigationActions = navigationActions,
+                    )
                 }
             }
         }
     }
 }
+
