d8d707304878e4fa284985053036aa191a450629
Lucas Israel <lisraelalves92@gmail.com>
2025-01-09 22:24:41 -0300

    removed spacer from OverviewColumn

--- app/src/main/java/br/com/lucasisrael/jetposemovies/details/presentation/components/OverviewColumn.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/details/presentation/components/OverviewColumn.kt
@@ -1,47 +1,30 @@
 package br.com.lucasisrael.jetposemovies.details.presentation.components
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import br.com.lucasisrael.jetposemovies.R
 
 @SuppressWarnings("FunctionNaming")
 @Composable
 fun OverviewColumn(
-    overview: String,
+    overview: String?,
     modifier: Modifier
 ) {
     Column(
         modifier = modifier
     ) {
         Text(
-            text = stringResource(R.string.overview)
-        )
-
-        Spacer(
-            modifier = Modifier
-                .padding(2.dp)
-        )
-
-        Text(
-            text = overview,
+            text = overview ?: stringResource(R.string.field_not_available),
             fontWeight = FontWeight.Normal,
             fontSize = 16.sp,
             modifier = Modifier
                 .fillMaxWidth()
         )
     }
-
-    Spacer(
-        modifier = Modifier.padding(bottom = 16.dp)
-    )
-
 }
