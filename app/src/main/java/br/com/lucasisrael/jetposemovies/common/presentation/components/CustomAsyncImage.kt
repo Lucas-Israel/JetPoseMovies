e418ae383ffda7d5d80369cbb436b64f707ac32c
Lucas Israel <lisraelalves92@gmail.com>
2025-01-14 19:43:36 -0300

    added GradientOrientation to CustomAsyncImage

--- app/src/main/java/br/com/lucasisrael/jetposemovies/common/presentation/components/CustomAsyncImage.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/common/presentation/components/CustomAsyncImage.kt
@@ -1,10 +1,13 @@
+@file:Suppress("FunctionNaming")
+
 package br.com.lucasisrael.jetposemovies.common.presentation.components
 
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawWithCache
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -12,24 +15,93 @@ import br.com.lucasisrael.jetposemovies.R
 import br.com.lucasisrael.jetposemovies.common.utils.constants.Constants.HALF_FLOAT
 import coil.compose.AsyncImage
 
-@SuppressWarnings("FunctionNaming")
 @Composable
 fun CustomAsyncImage(
     url: String?,
     title: String?,
     modifier: Modifier,
-    enableGradient: Boolean = false
-){
-    val color = MaterialTheme.colorScheme.background
-    val modified = if (enableGradient) {
+    gradientOrientation: GradientOrientation? = null,
+) {
+    val modified = remember {
+        isGradientModifier(
+            gradientOrientation = gradientOrientation,
+            modifier = modifier,
+        )
+    }
+
+    AsyncImage(
+        model = stringResource(
+            R.string.image_base_url,
+            url ?: stringResource(id = R.string.field_not_available)
+        ),
+        contentDescription = stringResource(
+            R.string.movie_image_from_the_genre,
+            title ?: stringResource(id = R.string.field_not_available)
+        ),
+        placeholder = painterResource(id = R.drawable.ic_launcher_foreground),
+        error = painterResource(id = R.drawable.ic_action_name),
+        contentScale = ContentScale.FillWidth,
+        modifier = modified
+    )
+}
+
+sealed class GradientOrientation {
+    abstract fun getBrush(): Brush
+    data class GradientBottom(val color: Color) : GradientOrientation() {
+        override fun getBrush(): Brush {
+            return customBrush(
+                color = color,
+                start = 0f,
+                end = Float.POSITIVE_INFINITY,
+            )
+        }
+    }
+
+    data class GradientTop(val color: Color) : GradientOrientation() {
+        override fun getBrush(): Brush {
+            return customBrush(
+                color = color,
+                start = Float.POSITIVE_INFINITY,
+                end = 0f
+            )
+        }
+    }
+
+    data class GradientStart(val color: Color) : GradientOrientation() {
+        override fun getBrush(): Brush {
+            return customBrush(
+                isVertical = false,
+                color = color,
+                start = Float.POSITIVE_INFINITY,
+                end = 0f
+            )
+        }
+    }
+
+    data class GradientEnd(val color: Color) : GradientOrientation() {
+        override fun getBrush(): Brush {
+            return customBrush(
+                isVertical = false,
+                color = color,
+                start = 0f,
+                end = Float.POSITIVE_INFINITY
+            )
+        }
+    }
+}
+
+private fun isGradientModifier(
+    gradientOrientation: GradientOrientation?,
+    modifier: Modifier,
+): Modifier {
+    return if (gradientOrientation != null) {
         modifier
             .drawWithCache {
                 onDrawWithContent {
                     drawContent()
                     drawRect(
-                        Brush.verticalGradient(
-                            HALF_FLOAT to color.copy(alpha = 0.25f),
-                            2f to color
+                        brushSelector(
+                            gradientOrientation = gradientOrientation,
                         )
                     )
                 }
@@ -37,13 +109,38 @@ fun CustomAsyncImage(
     } else {
         modifier
     }
+}
 
-    AsyncImage(
-        model =  stringResource(R.string.image_base_url, url ?: "" ),
-        contentDescription = stringResource(R.string.movie_image_from_the_genre, title ?: ""),
-        placeholder = painterResource(id = R.drawable.ic_launcher_foreground),
-        error = painterResource(id = R.drawable.ic_action_name),
-        contentScale = ContentScale.FillWidth,
-        modifier = modified
-    )
+private fun brushSelector(
+    gradientOrientation: GradientOrientation,
+): Brush {
+    return when (gradientOrientation) {
+        is GradientOrientation.GradientBottom -> gradientOrientation.getBrush()
+        is GradientOrientation.GradientEnd -> gradientOrientation.getBrush()
+        is GradientOrientation.GradientStart -> gradientOrientation.getBrush()
+        is GradientOrientation.GradientTop -> gradientOrientation.getBrush()
+    }
+}
+
+private fun customBrush(
+    color: Color,
+    start: Float,
+    end: Float,
+    isVertical: Boolean = true,
+): Brush {
+    return if (isVertical) {
+        Brush.verticalGradient(
+            HALF_FLOAT to color.copy(alpha = 0.25f),
+            2f to color,
+            startY = start,
+            endY = end,
+        )
+    } else {
+        Brush.horizontalGradient(
+            HALF_FLOAT to color.copy(alpha = 0.25f),
+            2f to color,
+            startX = start,
+            endX = end
+        )
+    }
 }
