61cab60b0e295d940bfae443c72bfddf2e864d48
Lucas Israel <lisraelalves92@gmail.com>
2025-01-14 15:55:13 -0300

    new converter to simplify the addition of videos in details

--- app/src/main/java/br/com/lucasisrael/jetposemovies/common/converters/Converters.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/common/converters/Converters.kt
@@ -1,6 +1,7 @@
 package br.com.lucasisrael.jetposemovies.common.converters
 
 import androidx.room.TypeConverter
+import br.com.lucasisrael.jetposemovies.details.models.remote.VideosItem
 import br.com.lucasisrael.jetposemovies.genres.models.local.GenreEntity
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
@@ -14,7 +15,18 @@ class Converters {
     }
 
     @TypeConverter
-    fun fromList(list: List<GenreEntity>): String {
+    fun toGenresJson(list: List<GenreEntity>): String {
+        return Gson().toJson(list)
+    }
+
+    @TypeConverter
+    fun fromVideos(value: String): List<VideosItem> {
+        val listType = object : TypeToken<List<VideosItem>>() {}.type
+        return Gson().fromJson(value, listType)
+    }
+
+    @TypeConverter
+    fun toVideosJson(list: List<VideosItem>): String {
         return Gson().toJson(list)
     }
 
