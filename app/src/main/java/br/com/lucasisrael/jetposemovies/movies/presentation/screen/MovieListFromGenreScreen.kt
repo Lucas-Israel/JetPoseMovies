b100b2b636182bf4de248ba1be7c7158c2057568
Lucas Israel <lisraelalves92@gmail.com>
2025-01-14 15:59:21 -0300

    small fix for MovieListFromGenreScreen when being rendered in a column

--- app/src/main/java/br/com/lucasisrael/jetposemovies/movies/presentation/screen/MovieListFromGenreScreen.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/movies/presentation/screen/MovieListFromGenreScreen.kt
@@ -4,7 +4,8 @@ import android.content.res.Configuration
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.LazyRow
@@ -13,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.stringResource
@@ -101,7 +103,7 @@ private fun MovieByGenreRow(
                     title = movie.title,
                     url = movie.posterPath,
                     modifier = Modifier
-                        .fillMaxWidth()
+                        .fillMaxSize()
                         .clickable {
                             navigationActions.toDetailsScreen(movieId = movie.id!!)
                         }
@@ -122,7 +124,10 @@ private fun MovieByGenreColumn(
     LazyColumn(
         contentPadding = PaddingValues(16.dp),
         verticalArrangement = Arrangement.spacedBy(16.dp),
-        state = listState
+        horizontalAlignment = Alignment.CenterHorizontally,
+        state = listState,
+        modifier = Modifier
+            .fillMaxSize()
     ) {
         items(count = movies.itemCount) { index ->
             val movie = movies[index]
@@ -131,7 +136,7 @@ private fun MovieByGenreColumn(
                     title = movie.title,
                     url = movie.posterPath,
                     modifier = Modifier
-                        .fillMaxWidth()
+                        .fillMaxHeight()
                         .clickable {
                             navigationActions.toDetailsScreen(movieId = movie.id!!)
                         }
