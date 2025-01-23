8ddb977cd9a11a2258ac82edd8aa4a264ea18092
Lucas Israel <lisraelalves92@gmail.com>
2025-01-23 17:12:05 -0300

    using CustomButton, hiltViewModel() now is used on its parent.

--- app/src/main/java/br/com/lucasisrael/jetposemovies/genres/presentation/components/GenresComponent.kt
+++ app/src/main/java/br/com/lucasisrael/jetposemovies/genres/presentation/components/GenresComponent.kt
@@ -1,69 +1,44 @@
 package br.com.lucasisrael.jetposemovies.genres.presentation.components
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.paging.compose.collectAsLazyPagingItems
+import androidx.paging.compose.LazyPagingItems
 import br.com.lucasisrael.jetposemovies.common.navigation.NavigationActions
-import br.com.lucasisrael.jetposemovies.genres.presentation.viewmodel.GenresViewModel
-import kotlinx.serialization.Serializable
-
-@Serializable
-object GenresComponent
+import br.com.lucasisrael.jetposemovies.common.presentation.components.CustomButton
+import br.com.lucasisrael.jetposemovies.genres.models.domain.GenreDomain
 
 @Suppress("FunctionNaming")
 @Composable
 fun GenresComponent(
     navigationActions: NavigationActions,
-    viewModel: GenresViewModel = hiltViewModel(),
+    genres: LazyPagingItems<GenreDomain>,
 ) {
 
-    val data = viewModel.pagingFlow.collectAsLazyPagingItems()
-
     LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
         modifier = Modifier
             .padding(8.dp)
     ) {
-        items(count = data.itemCount) { index ->
-            val genre = data[index]
+        items(count = genres.itemCount) { index ->
+            val genre = genres[index]
             if (genre != null) {
-                Box(
+                CustomButton(
+                    onClick = {
+                    navigationActions.toMovieGenreScreen(
+                        genreId = genre.id,
+                        genreName = genre.name
+                    )},
                     modifier = Modifier
-                        .padding(4.dp)
-                        .clickable {
-                            navigationActions.toMovieGenreScreen(
-                                genre.id,
-                                genre.name
-                            )
-                        }
                 ) {
-                    Box(
-                        contentAlignment = Alignment.Center,
+                    Text(
+                        text = genre.name,
                         modifier = Modifier
-                            .clip(RoundedCornerShape(6.dp))
-                            .background(MaterialTheme.colorScheme.surfaceContainerHigh)
-                            .padding(12.dp)
-                            .width(150.dp)
-                    ) {
-                        Text(
-                            text = genre.name,
-                            modifier = Modifier
-                        )
-
-                    }
-
+                    )
                 }
             }
         }
